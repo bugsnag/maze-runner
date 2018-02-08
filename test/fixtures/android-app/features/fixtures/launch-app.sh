6076adb3507009245ff72f4cb80f2ef7d6c223e4
fractalwrench <fractalwrench@gmail.com>
2018-02-08 15:23:29 +0000

    setup launch command with intent

--- test/fixtures/android-app/features/fixtures/launch-app.sh
+++ test/fixtures/android-app/features/fixtures/launch-app.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#     "EVENT_TYPE=$EVENT_TYPE" \
-#     "BUGSNAG_API_KEY=$BUGSNAG_API_KEY" \
-#     "MOCK_API_PATH=http://localhost:$MOCK_API_PORT"
-
-
 function poll_app() {
   # Detect whether app is still in the foreground (app kills its own process when completed)
   while [[ `adb shell dumpsys activity | grep "Proc # 0" | grep "com.bugsnag.android.mazerunner"` ]];
@@ -13,20 +8,17 @@ function poll_app() {
   done
 }
 
+echo "Killing any extant app process"
+adb shell am force-stop com.bugsnag.android.mazerunner
+
+# TODO doesn't need to run for each step
 # Clear any existing data
 echo "Launching MainActivity"
 adb shell clear com.bugsnag.android.mazerunner
 
+
 echo "Launching MainActivity"
-adb shell am start -n com.bugsnag.android.mazerunner/com.bugsnag.android.mazerunner.MainActivity
+adb shell am start -n com.bugsnag.android.mazerunner/com.bugsnag.android.mazerunner.MainActivity --es EVENT_TYPE "$EVENT_TYPE"
 poll_app
 
-echo "Killing app process"
-adb shell am force-stop com.bugsnag.android.mazerunner
-
-# launch again, with session sending enabled
-sleep 2 # wait for app to close
-echo "Launching MainActivity again"
-adb shell am start -n com.bugsnag.android.mazerunner/com.bugsnag.android.mazerunner.MainActivity --es EVENT_TYPE "boo"
-poll_app
-echo "Android App finished"
+echo "Ran Android Test case"

24f53756011b1247a166b70fe65ddf1c40b4fc89
Delisa Mason <iskanamagus@gmail.com>
2018-09-25 14:26:19 -0700

    Support optional request index for error reporting validation

--- lib/features/steps/error_reporting_steps.rb
+++ lib/features/steps/error_reporting_steps.rb
@@ -1,21 +1,22 @@
-Then(/^the request is(?: a)? valid for the error reporting API$/) do
+Then(/^(?:the )?request (?:(\d+) )?is(?: a)? valid for the error reporting API$/) do |index|
+  index = 0 if index.nil?
   steps %Q{
-    Then the "Bugsnag-API-Key" header is not null
-    And the "Content-Type" header equals "application/json"
-    And the "Bugsnag-Payload-Version" header equals one of:
+    Then the "Bugsnag-API-Key" header is not null for request #{index}
+    And the "Content-Type" header equals "application/json" for request #{index}
+    And the "Bugsnag-Payload-Version" header for request #{index} equals one of:
       | 4   |
       | 4.0 |
-    And the "Bugsnag-Sent-At" header is a timestamp
+    And the "Bugsnag-Sent-At" header is a timestamp for request #{index}
 
-    And the payload field "notifier.name" is not null
-    And the payload field "notifier.url" is not null
-    And the payload field "notifier.version" is not null
-    And the payload field "events" is a non-empty array
+    And the payload field "notifier.name" is not null for request #{index}
+    And the payload field "notifier.url" is not null for request #{index}
+    And the payload field "notifier.version" is not null for request #{index}
+    And the payload field "events" is a non-empty array for request #{index}
 
-    And each element in payload field "events" has "severity"
-    And each element in payload field "events" has "severityReason.type"
-    And each element in payload field "events" has "unhandled"
-    And each element in payload field "events" has "exceptions"
+    And each element in payload field "events" has "severity" for request #{index}
+    And each element in payload field "events" has "severityReason.type" for request #{index}
+    And each element in payload field "events" has "unhandled" for request #{index}
+    And each element in payload field "events" has "exceptions" for request #{index}
   }
 end
 Then("the event {string} is true") do |field|

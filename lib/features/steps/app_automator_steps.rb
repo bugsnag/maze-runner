92e8a521986103c9b0d4e529488707149a64ee70
Alex Moinet <alex@bugsnag.com>
2019-05-09 10:18:42 +0100

    Browserstack drivers: Use global driver over @driver

--- lib/features/steps/app_automator_steps.rb
+++ lib/features/steps/app_automator_steps.rb
@@ -1,9 +1,9 @@
 Given("the element {string} is present") do |element_id|
-  @driver.wait_on_element(element_id)
+  $driver.wait_on_element(element_id)
 end
 When("I click the element {string}") do |element_id|
-  @driver.click_element(element_id)
+  $driver.click_element(element_id)
 end
 Given("I send the app to the background for {int} seconds") do |timeout|
-  @driver.timeout_app(timeout)
+  $driver.timeout_app(timeout)
 end

df7fa882cb90d8e6caef524dc921a8d29c50e2b9
Alex Moinet <alex@bugsnag.com>
2019-05-13 16:19:45 +0100

    Browserstack-driver: Remove debugging steps

--- test/fixtures/browserstack-app/features/support/env.rb
+++ test/fixtures/browserstack-app/features/support/env.rb
@@ -7,11 +7,6 @@ bs_local_id = ENV['BROWSER_STACK_LOCAL_IDENTIFIER'] || 'maze_browser_stack_test_
 bs_device = 'ANDROID_9'
 app_location = 'app/build/outputs/apk/release/app-release.apk'
 
-pp "Beginning of envs"
-pp bs_username
-pp bs_access_key
-pp "End of envs"
-
 AfterConfiguration do |config|
   $driver = AppAutomateDriver.new(bs_username, bs_access_key, bs_local_id)
   $driver.start_driver(bs_device, app_location)

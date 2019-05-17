0a3c1d42f700f8449f5e71c2d5fb9a9586e171a8
Alex Moinet <alex@bugsnag.com>
2019-05-17 13:06:25 +0100

    BrowserStack-driver: Adapt integration test to target driver correctly

--- test/fixtures/browserstack-app/features/support/env.rb
+++ test/fixtures/browserstack-app/features/support/env.rb
@@ -6,6 +6,5 @@ bs_device = 'ANDROID_9'
 app_location = 'app/build/outputs/apk/release/app-release.apk'
 
 AfterConfiguration do |config|
-  $driver = AppAutomateDriver.new(bs_username, bs_access_key, bs_local_id)
-  $driver.start_driver(bs_device, app_location)
+  AppAutomateDriver.new(bs_username, bs_access_key, bs_local_id, bs_device, app_location)
 end

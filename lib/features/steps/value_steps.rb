231111139aae278500d95783c35b0b80a317052f
Alex Moinet <alex@bugsnag.com>
2019-08-08 11:47:13 +0100

    Fix comparisons: Ensure key is used over field

--- lib/features/steps/value_steps.rb
+++ lib/features/steps/value_steps.rb
@@ -1,18 +1,18 @@
 Then("the payload field {string} is stored as the value {string}") do |field, key|
   value = read_key_path(Server.current_request[:body], field)
-  Store.values[field] = value.dup
+  Store.values[key] = value.dup
 end
 
 Then("the payload field {string} equals the stored value {string}") do |field, key|
   payload_value = read_key_path(Server.current_request[:body], field)
-  stored_value = Store.values[field]
+  stored_value = Store.values[key]
   result = value_compare(payload_value, stored_value)
   assert_true(result.equal?, "Payload value: #{payload_value} does not equal stored value: #{stored_value}")
 end
 
 Then("the payload field {string} does not equal the stored value {string}") do |field, key|
   payload_value = read_key_path(Server.current_request[:body], field)
-  stored_value = Store.values[field]
+  stored_value = Store.values[key]
   result = value_compare(payload_value, stored_value)
   assert_false(result.equal?, "Payload value: #{payload_value} equals stored value: #{stored_value}")
 end

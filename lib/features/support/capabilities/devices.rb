9263bc4cae1a0a984dbb9995ae61a45609a14969
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-02-17 16:55:26 +0000

    chore(devices): maintain backward compatibility for device names

--- lib/features/support/capabilities/devices.rb
+++ lib/features/support/capabilities/devices.rb
@@ -1,82 +1,100 @@
 # Provides a source of capabilities used to run tests against specific BrowserStack devices
 #noinspection RubyStringKeysInHashInspection
 class Devices
+
   # The hash of device capabilities, accessible by simple names
+  ANDROID_10_0 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Pixel 4',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '10.0',
+      'browserstack.appium_version' => '1.15.0'
+  }
+  ANDROID_9_0 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Pixel 3',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '9.0'
+  }
+  ANDROID_8_1 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Samsung Galaxy Note 9',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '8.1'
+  }
+  ANDROID_8_0 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Pixel 2',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '8.0'
+  }
+  ANDROID_7_1 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Pixel',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '7.1'
+  }
+  ANDROID_6_0 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Nexus 6',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '6.0'
+  }
+  ANDROID_5_0 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Nexus 6',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '5.0'
+  }
+  ANDROID_4_4 = {
+      'autoGrantPermissions' => 'true',
+      'device' => 'Google Nexus 5',
+      'platformName' => 'Android',
+      'os' => 'android',
+      'os_version' => '4.4'
+  }
+  IOS_12 = {
+      'device' => 'iPhone 8',
+      'platformName' => 'iOS',
+      'os' => 'ios',
+      'os_version' => '12'
+  }
+  IOS_11 = {
+      'device' => 'iPhone 8',
+      'platformName' => 'iOS',
+      'os' => 'ios',
+      'os_version' => '11'
+  }
+  IOS_10 = {
+      'device' => 'iPhone 7',
+      'platformName' => 'iOS',
+      'os' => 'ios',
+      'os_version' => '10'
+  }
   DEVICE_HASH = {
-      'ANDROID_10' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Pixel 4',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '10.0',
-          'browserstack.appium_version' => '1.15.0'
-      },
-      'ANDROID_9' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Pixel 3',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '9.0'
-      },
-      'ANDROID_8_1' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Samsung Galaxy Note 9',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '8.1'
-      },
-      'ANDROID_8_0' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Pixel 2',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '8.0'
-      },
-      'ANDROID_7_1' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Pixel',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '7.1'
-      },
-      'ANDROID_6' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Nexus 6',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '6.0'
-      },
-      'ANDROID_5' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Nexus 6',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '5.0'
-      },
-      'ANDROID_4_4' => {
-          'autoGrantPermissions' => 'true',
-          'device' => 'Google Nexus 5',
-          'platformName' => 'Android',
-          'os' => 'android',
-          'os_version' => '4.4'
-      },
-      'IOS_12' => {
-          'device' => 'iPhone 8',
-          'platformName' => 'iOS',
-          'os' => 'ios',
-          'os_version' => '12'
-      },
-      'IOS_11' => {
-          'device' => 'iPhone 8',
-          'platformName' => 'iOS',
-          'os' => 'ios',
-          'os_version' => '11'
-      },
-      'IOS_10' => {
-          'device' => 'iPhone 7',
-          'platformName' => 'iOS',
-          'os' => 'ios',
-          'os_version' => '10'
-      }
+      'ANDROID_10_0' => ANDROID_10_0,
+      'ANDROID_9_0' => ANDROID_9_0,
+      'ANDROID_8_1' => ANDROID_8_1,
+      'ANDROID_8_0' => ANDROID_8_0,
+      'ANDROID_7_1' => ANDROID_7_1,
+      'ANDROID_6_0' => ANDROID_6_0,
+      'ANDROID_4_4' => ANDROID_4_4,
+      'IOS_12' => IOS_12,
+      'IOS_11' => IOS_11,
+      'IOS_10' => IOS_10,
+      # Deprecated entries
+      'ANDROID_9' => ANDROID_9_0,
+      'ANDROID_8' => ANDROID_8_0,
+      'ANDROID_7' => ANDROID_7_1,
+      'ANDROID_6' => ANDROID_6_0,
+      'ANDROID_5' => ANDROID_5_0,
+      'ANDROID_4' => ANDROID_4_4
   }
 end

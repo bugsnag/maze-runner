76765a600dc4ce06bdf21dd93034463772ef884c
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-08-20 19:50:21 +0100

    Catch WebDriverError too

--- lib/features/support/app_automate_driver.rb
+++ lib/features/support/app_automate_driver.rb
@@ -78,11 +78,12 @@ class AppAutomateDriver < Appium::Driver
       wait.until { find_element(@element_locator, element_id).displayed? }
     rescue Selenium::WebDriver::Error::TimeoutError
       false
-    rescue Selenium::WebDriver::Error::UnknownError
-      # TODO Limit the number of retries
+
+    rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
+      # TODO Just hacking around currently
       $logger.warn 'Appium UnknownError occurred - restarting driver'
       $driver.restart
-      wait_for_element(element_id, timeout, retry_if_stale)
+      wait_for_element(element_id, timeout, false)
     rescue Selenium::WebDriver::Error::StaleElementReferenceError => stale_error
       if retry_if_stale
         wait_for_element(element_id, timeout, false)

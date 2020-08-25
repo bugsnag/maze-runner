8ca5d1e337d497e02a9f4152b8dd228d3592f628
Alex Moinet <alex@bugsnag.com>
2020-08-25 11:58:49 +0100

    Add nil check to log_request method

--- lib/features/support/servlet.rb
+++ lib/features/support/servlet.rb
@@ -54,6 +54,8 @@ class Servlet < WEBrick::HTTPServlet::AbstractServlet
     $logger.debug "URI: #{request.unparsed_uri}"
     $logger.debug "HEADERS: #{request.raw_header}"
     case request['Content-Type']
+    when nil
+      return
     when %r{^multipart/form-data; boundary=([^;]+)}
       boundary = WEBrick::HTTPUtils.dequote(Regexp.last_match(1))
       body = WEBrick::HTTPUtils.parse_form_data(request.body(), boundary)

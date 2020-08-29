9e417f899b76cb9f693b3c589c71f2256c6f2255
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-08-29 21:14:35 +0100

    Rough initial idea of new abstraction

--- lib/features/support/server.rb
+++ lib/features/support/server.rb
@@ -1,86 +1,87 @@
 # frozen_string_literal: true
 
-require 'webrick'
 require 'json'
+require 'singleton'
+require 'webrick'
 require_relative './servlet'
 require_relative './logger'
 
-# This port number is semi-arbitrary. It doesn't matter for the sake of
-# the application what it is, but there are some constraints due to some
-# of the environments that we know this will be used in - namely, driving
-# remote browsers on BrowserStack. The ports/ranges that Safari will access
-# on "localhost" urls are restricted to the following:
-#
-#   80, 3000, 4000, 5000, 8000, 8080 or 9000-9999
-#   [ from https://stackoverflow.com/a/28678652 ]
-#
-MOCK_API_PORT = 9339
-
 # Receives and stores requests through a WEBrick HTTPServer
 class Server
-  class << self
-    # Whether the server thread is running
-    #
-    # @return [Boolean] If the server is running
-    def running?
-      @thread&.alive?
-    end
+  include Singleton
 
-    # An array of requests received.
-    # Each request is hash consisting of:
-    #   body: The parsed body of the request
-    #   request: The original HTTPRequest object
-    #
-    # @return [Array] An array of received requests
-    def stored_requests
-      @stored_requests ||= []
-    end
+  # There are some constraints on the port from driving remote browsers on BrowserStack.
+  # E.g. the ports/ranges that Safari will access on "localhost" urls are restricted to the following:
+  #   80, 3000, 4000, 5000, 8000, 8080 or 9000-9999 [ from https://stackoverflow.com/a/28678652 ]
+  PORT = 9339
 
-    # The first request received by the server.
-    #
-    # @return [Hash|nil] The first request
-    def current_request
-      stored_requests.first
-    end
+  def initialize
+    @all_notify_requests = []
+    @remaining_notify_requests = []
+  end
 
-    # Starts the WEBrick server in a separate thread
-    def start_server
-      attempts = 0
-      $logger.info 'Starting mock server'
-      loop do
-        @thread = Thread.new do
-          server = WEBrick::HTTPServer.new(
-            Port: MOCK_API_PORT,
-            Logger: $logger,
-            AccessLog: []
-          )
-          server.mount '/', Servlet
-          server.start
-        rescue StandardError => e
-          $logger.warn "Failed to start mock server: #{e.message}"
-        ensure
-          server&.shutdown
-        end
+  # Whether the server thread is running
+  #
+  # @return [Boolean] If the server is running
+  def running?
+    @thread&.alive?
+  end
 
-        # Need a short sleep here as a dying thread is still alive momentarily
-        sleep 1
-        break if running?
+  def store_notify_request(request)
+    @all_notify_requests.append request.clone
+    @remaining_notify_requests.append request.clone
+  end
 
-        # Bail out after 3 attempts
-        attempts += 1
-        raise 'Too many failed attempts to start mock server' if attempts == 3
+  def take_next_notify_request
+    @remaining_notify_requests.shift
+  end
 
-        # Failed to start - sleep before retrying
-        $logger.info 'Retrying in 5 seconds'
-        sleep 5
+  def all_notify_requests
+    @all_notify_requests.clone.freeze
+  end
+
+  def clear_all_requests
+    @all_notify_requests.clear
+    @remaining_notify_requests.clear
+  end
+
+  # Starts the WEBrick server in a separate thread
+  def start_server
+    attempts = 0
+    $logger.info 'Starting mock server'
+    loop do
+      @thread = Thread.new do
+        server = WEBrick::HTTPServer.new(
+          Port: MOCK_API_PORT,
+          Logger: $logger,
+          AccessLog: []
+        )
+        server.mount '/', Servlet
+        server.start
+      rescue StandardError => e
+        $logger.warn "Failed to start mock server: #{e.message}"
+      ensure
+        server&.shutdown
       end
-    end
 
-    # Stops the WEBrick server thread if it's running
-    def stop_server
-      @thread&.kill if @thread&.alive?
-      @thread = nil
+      # Need a short sleep here as a dying thread is still alive momentarily
+      sleep 1
+      break if running?
+
+      # Bail out after 3 attempts
+      attempts += 1
+      raise 'Too many failed attempts to start mock server' if attempts == 3
+
+      # Failed to start - sleep before retrying
+      $logger.info 'Retrying in 5 seconds'
+      sleep 5
     end
+  end
+
+  # Stops the WEBrick server thread if it's running
+  def stop_server
+    @thread&.kill if @thread&.alive?
+    @thread = nil
   end
 end
 

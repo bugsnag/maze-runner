d895b64eee37b60dfc787d2587501d8e3085ab7c
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-04 12:33:03 +0100

    Add additional Android devices to choose from

--- lib/features/support/capabilities/devices.rb
+++ lib/features/support/capabilities/devices.rb
@@ -2,68 +2,44 @@
 # noinspection RubyStringKeysInHashInspection
 class Devices
 
+  class << self
+
+    def make_android_hash(device, version)
+      {
+        'device' => device,
+        'os_version' => version,
+        'autoGrantPermissions' => 'true',
+        'platformName' => 'Android',
+        'os' => 'android'
+      }.freeze
+    end
+  end
+
   # The hash of device capabilities, accessible by simple names
-  ANDROID_10_0 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Pixel 4',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '10.0'
-  }.freeze
-  ANDROID_9_0 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Pixel 3',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '9.0'
-  }.freeze
-  ANDROID_8_1 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Samsung Galaxy Note 9',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '8.1'
-  }.freeze
-  ANDROID_8_0 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Pixel 2',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '8.0'
-  }.freeze
-  ANDROID_7_1 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Pixel',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '7.1'
-  }.freeze
-  ANDROID_6_0 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Nexus 6',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '6.0'
-  }.freeze
-  ANDROID_5_0 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Nexus 6',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '5.0'
-  }.freeze
-  ANDROID_4_4 = {
-    'autoGrantPermissions' => 'true',
-    'device' => 'Google Nexus 5',
-    'platformName' => 'Android',
-    'os' => 'android',
-    'os_version' => '4.4'
-  }.freeze
+  ANDROID_10_0 = Devices.make_android_hash 'Google Pixel 4', '10.0'
+  ANDROID_9_0 = Devices.make_android_hash 'Google Pixel 3', '9.0'
+  ANDROID_8_1_GALAXY_NOTE_9 = Devices.make_android_hash 'Samsung Galaxy Note 9', '8.1'
+  ANDROID_8_1_GALAXY_J7_PRIME = Devices.make_android_hash 'Samsung Galaxy J7 Prime', '8.1'
+  ANDROID_8_1_GALAXY_TAB_S4 = Devices.make_android_hash 'Samsung Galaxy Tab S4', '8.1'
+  ANDROID_8_1 = ANDROID_8_1_GALAXY_NOTE_9
+  ANDROID_8_0_PIXEL = Devices.make_android_hash 'Google Pixel', '8.0'
+  ANDROID_8_0_PIXEL_2 = Devices.make_android_hash 'Google Pixel 2', '8.0'
+  ANDROID_8_0_GALAXY_S9 = Devices.make_android_hash 'Samsung Galaxy S9', '8.0'
+  ANDROID_8_0_GALAXY_S9_PLUS = Devices.make_android_hash 'Samsung Galaxy S9 Plus', '8.0'
+  ANDROID_8_0_GALAXY_TAB_S3 = Devices.make_android_hash 'Samsung Galaxy Tab S3', '8.0'
+  ANDROID_8_0 = ANDROID_8_0_PIXEL_2
+  ANDROID_7_1 = Devices.make_android_hash 'Google Pixel', '7.1'
+  ANDROID_6_0_MOTO_X_2ND_GEN = Devices.make_android_hash 'Motorola Moto X 2nd Gen', '6.0'
+  ANDROID_6_0_NEXUS_6 = Devices.make_android_hash 'Google Nexus 6', '6.0'
+  ANDROID_6_0_GALAXY_S7 = Devices.make_android_hash 'Samsung Galaxy S7', '6.0'
+  ANDROID_6_0 = ANDROID_6_0_NEXUS_6
+  ANDROID_5_0 = Devices.make_android_hash 'Google Nexus 6', '5.0'
+  ANDROID_4_4 = Devices.make_android_hash 'Google Nexus 5', '4.4'
   IOS_13 = {
-      'device' => 'iPhone 8',
-      'platformName' => 'iOS',
-      'os' => 'ios',
-      'os_version' => '13'
+    'device' => 'iPhone 8',
+    'platformName' => 'iOS',
+    'os' => 'ios',
+    'os_version' => '13'
   }.freeze
   IOS_12 = {
     'device' => 'iPhone 8',
@@ -87,9 +63,20 @@ class Devices
     'ANDROID_10_0' => ANDROID_10_0,
     'ANDROID_9_0' => ANDROID_9_0,
     'ANDROID_8_1' => ANDROID_8_1,
+    'ANDROID_8_1_GALAXY_J7_PRIME' => ANDROID_8_1_GALAXY_J7_PRIME,
+    'ANDROID_8_1_GALAXY_NOTE_9' => ANDROID_8_1_GALAXY_NOTE_9,
+    'ANDROID_8_1_GALAXY_TAB_S4' => ANDROID_8_1_GALAXY_TAB_S4,
     'ANDROID_8_0' => ANDROID_8_0,
+    'ANDROID_8_0_GALAXY_S9' => ANDROID_8_0_GALAXY_S9,
+    'ANDROID_8_0_GALAXY_S9_PLUS' => ANDROID_8_0_GALAXY_S9_PLUS,
+    'ANDROID_8_0_GALAXY_TAB_S3' => ANDROID_8_0_GALAXY_TAB_S3,
+    'ANDROID_8_0_PIXEL' => ANDROID_8_0_PIXEL,
+    'ANDROID_8_0_PIXEL_2' => ANDROID_8_0_PIXEL_2,
     'ANDROID_7_1' => ANDROID_7_1,
     'ANDROID_6_0' => ANDROID_6_0,
+    'ANDROID_6_0_NEXUS_6' => ANDROID_6_0_NEXUS_6,
+    'ANDROID_6_0_GALAXY_S7' => ANDROID_6_0_GALAXY_S7,
+    'ANDROID_6_0_MOTO_X_2ND_GEN' => ANDROID_6_0_MOTO_X_2ND_GEN,
     'ANDROID_4_4' => ANDROID_4_4,
     'IOS_13' => IOS_13,
     'IOS_12' => IOS_12,

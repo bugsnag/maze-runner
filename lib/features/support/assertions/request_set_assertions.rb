b07b44507fc26cadb6a2af5f76395f250750c39a
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-12 22:30:30 +0100

    Refactored for ease of unit testing.  Also ensure that only one request matches each table row.

--- lib/features/support/assertions/request_set_assertions.rb
+++ lib/features/support/assertions/request_set_assertions.rb
@@ -14,38 +14,47 @@ class RequestSetAssertions
     #   - headings can be provided as key paths (e.g. events.0.breadcrumbs.0.name)
     #   - table values can be written as "null" for nil
     def assert_requests_match(requests, table)
-      request_count = requests.count
+      matches = matching_rows requests, table
+      return if matches.length == table.hashes.length
+
+      # Not all matched - log diagnostic before failing assertion
+      $logger.error "Only #{matches.length} of #{requests.length} matched:"
+      $logger.info matches.keys.sort
+      matches.sort.to_h.each do |row, request|
+        $logger.info "#{table.rows[row]} matched by request element #{matches[request]}"
+      end
+      assert_equal(requests.length, matches.length, 'Not all requests matched a row in the table.')
+    end
+
+    # Given arrays of requests and table-based criteria, determines which rows of the table
+    # are satisfied by one of the requests.  Where multiple rows in the table specify the same
+    # criteria, there must be multiple requests provided to satisfy each.
+    #
+    # @param requests [Hash[]] Requests to check
+    # @param table [Cucumber::MultilineArgument::DataTable] Table of expected values, where:
+    #   - headings can be provided as key paths (e.g. events.0.breadcrumbs.0.name)
+    #   - table values can be written as "null" for nil
+    # @return [Hash] A hash of row to request indexes, indicating the first request matching each row.
+    #   E.g. {0 => 2} means that the first row was satisfied by the 3rd request.
+    def matching_rows(requests, table)
+      request_count = requests.length
       assert_equal(table.hashes.length, requests.length, 'Number of requests do not match number of entries in table.')
 
       # iterate through each row in the table. exactly 1 request should match each row.
-      matches = []
-      table.hashes.each do |row|
-        requests.each do |request|
+      row_to_request_matches = {}
+      table.hashes.each_with_index do |row, row_index|
+        requests.each_with_index do |request, request_index|
+          # Skip if row already matched
+          next if row_to_request_matches.values.include? request_index
           # Skip if no body in this request
           next unless request.key?(:body)
           next unless request_matches_row(request[:body], row)
 
-          # Record with row the request matched against
-          matches << {
-            body: request[:body],
-            row: row
-          }
+          # Record the match
+          row_to_request_matches[row_index] = request_index
         end
       end
-      if request_count != matches.length
-        # Log requests that matched
-        $logger.error "#{matches.length} of #{request_count} requests matched"
-        matches.each do |match|
-          $logger.info "#{match[:row]} was matched by:"
-          LogUtil.log_hash(Logger::Severity::INFO, match[:body])
-        end
-        $logger.info 'All requests checked:'
-        requests.each_with_index do |request, index|
-          $logger.info "Request #{index + 1} of #{requests.length}"
-          LogUtil.log_hash Logger::Severity::INFO, request[:body]
-        end
-      end
-      assert_equal(request_count, matches.length, 'Not all requests matched a row in the table.')
+      row_to_request_matches
     end
 
     def request_matches_row(body, row)
@@ -56,9 +65,7 @@ class RequestSetAssertions
         unless obs_val.nil?
           if expected_value[0] == '/' && expected_value[-1] == '/'
             # Treat as regexp
-            regex_string = expected_value[1, expected_value.length - 2]
-            $logger.info "Building a regexp from #{regex_string}"
-            regex = Regexp.new regex_string
+            regex = Regexp.new expected_value[1, expected_value.length - 2]
             next if regex.match? obs_val.to_s # Value matches regex
           elsif expected_value.eql? obs_val.to_s
             # Values match

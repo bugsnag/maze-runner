a1b367400e8f74d6c4d99ac3bad7fe173c0e1d5f
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-16 20:41:20 +0100

    Updated for new endpoints

--- test/fixtures/proxy/features/scripts/http_with_creds.sh
+++ test/fixtures/proxy/features/scripts/http_with_creds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -d @features/fixtures/payload.json -H "Content-Type: application/json" -U user:password -x http://localhost:9000 http://localhost:9339
+curl -X POST -d @features/fixtures/payload.json -H "Content-Type: application/json" -U user:password -x http://localhost:9000 http://localhost:9339/notify

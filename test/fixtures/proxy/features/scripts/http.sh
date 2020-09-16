5f0c91d192e024c8bc6fff6fb0701ba39b70947a
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-16 20:41:20 +0100

    Updated for new endpoints

--- test/fixtures/proxy/features/scripts/http.sh
+++ test/fixtures/proxy/features/scripts/http.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -d @features/fixtures/payload.json -H "Content-Type: application/json" -x http://localhost:9000 http://localhost:9339
+curl -X POST -d @features/fixtures/payload.json -H "Content-Type: application/json" -x http://localhost:9000 http://localhost:9339/notify

a0c4d8f90f5c7fa87e172768cdef515cbff5cc24
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-24 21:32:32 +0100

    Convert remaining uses of $driver to MazeRunner.driver and fix some Rubocop violations

--- lib/features/steps/app_automator_steps.rb
+++ lib/features/steps/app_automator_steps.rb
@@ -4,7 +4,7 @@
 # Requires a running Appium driver
 #
 # @step_input element_id [String] The locator id
-Given("the element {string} is present") do |element_id|
+Given('the element {string} is present') do |element_id|
   present = MazeRunner.driver.wait_for_element(element_id)
   assert(present, "The element #{element_id} could not be found")
 end
@@ -14,8 +14,8 @@ end
 #
 # @step_input element_id [String] The locator id
 # @step_input timeout [Int] The number of seconds to wait before timing out
-Given("the element {string} is present within {int} seconds") do |element_id, timeout|
-  present = $driver.wait_for_element(element_id, timeout)
+Given('the element {string} is present within {int} seconds') do |element_id, timeout|
+  present = MazeRunner.driver.wait_for_element(element_id, timeout)
   assert(present, "The element #{element_id} could not be found")
 end
 
@@ -23,24 +23,24 @@ end
 # Requires a running Appium driver
 #
 # @step_input element_id [String] The locator id
-When("I click the element {string}") do |element_id|
-  $driver.click_element(element_id)
+When('I click the element {string}') do |element_id|
+  MazeRunner.driver.click_element(element_id)
 end
 
 # Sends the app to the background for a number of seconds
 # Requires a running Appium driver
 #
 # @step_input timeout [Integer] The amount of time the app is in the background in seconds
-When("I send the app to the background for {int} seconds") do |timeout|
-  $driver.background_app(timeout)
+When('I send the app to the background for {int} seconds') do |timeout|
+  MazeRunner.driver.background_app(timeout)
 end
 
 # Clears a given element
 # Requires a running Appium driver
 #
 # @step_input element_id [String] The locator id
-When("I clear the element {string}") do |element_id|
-  $driver.clear(element_id, keys)
+When('I clear the element {string}') do |element_id|
+  MazeRunner.driver.clear(element_id, keys)
 end
 
 # Sends keys to a given element
@@ -48,8 +48,8 @@ end
 #
 # @step_input keys [String] The keys to send to the element
 # @step_input element_id [String] The locator id
-When("I send the keys {string} to the element {string}") do |keys, element_id|
-  $driver.send_keys_to_element(element_id, keys)
+When('I send the keys {string} to the element {string}') do |keys, element_id|
+  MazeRunner.driver.send_keys_to_element(element_id, keys)
 end
 
 # Tests that the given payload value is correct for the target BrowserStack platform.
@@ -64,7 +64,7 @@ end
 #
 # @step_input field_path [String] The field to test
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the payload field {string} equals the platform-dependent string:") do |field_path, platform_values|
+Then('the payload field {string} equals the platform-dependent string:') do |field_path, platform_values|
   testStringPlatformValues(field_path, platform_values)
 end
 
@@ -72,7 +72,7 @@ end
 #
 # @step_input field_path [String] The field to test, prepended with "events.0"
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the event {string} equals the platform-dependent string:") do |field_path, platform_values|
+Then('the event {string} equals the platform-dependent string:') do |field_path, platform_values|
   testStringPlatformValues("events.0.#{field_path}", platform_values)
 end
 
@@ -88,7 +88,7 @@ end
 #
 # @step_input field_path [String] The field to test
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the payload field {string} equals the platform-dependent numeric:") do |field_path, platform_values|
+Then('the payload field {string} equals the platform-dependent numeric:') do |field_path, platform_values|
   testNumericPlatformValues(field_path, platform_values)
 end
 
@@ -96,7 +96,7 @@ end
 #
 # @step_input field_path [String] The field to test, prepended with "events.0"
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the event {string} equals the platform-dependent numeric:") do |field_path, platform_values|
+Then('the event {string} equals the platform-dependent numeric:') do |field_path, platform_values|
   testNumericPlatformValues("events.0.#{field_path}", platform_values)
 end
 
@@ -112,7 +112,7 @@ end
 #
 # @step_input field_path [String] The field to test
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the payload field {string} equals the platform-dependent boolean:") do |field_path, platform_values|
+Then('the payload field {string} equals the platform-dependent boolean:') do |field_path, platform_values|
   testBooleanPlatformValues(field_path, platform_values)
 end
 
@@ -120,7 +120,7 @@ end
 #
 # @step_input field_path [String] The field to test, prepended with "events.0"
 # @step_input platform_values [DataTable] A table of acceptable values for each platform
-Then("the event {string} equals the platform-dependent boolean:") do |field_path, platform_values|
+Then('the event {string} equals the platform-dependent boolean:') do |field_path, platform_values|
   testBooleanPlatformValues("events.0.#{field_path}", platform_values)
 end
 
@@ -129,22 +129,24 @@ end
 #
 # @step_input keys [String] The keys to send to the element
 # @step_input element_id [String] The locator id
-When("I clear and send the keys {string} to the element {string}") do |keys, element_id|
-  $driver.clear_and_send_keys_to_element(element_id, keys)
+When('I clear and send the keys {string} to the element {string}') do |keys, element_id|
+  MazeRunner.driver.clear_and_send_keys_to_element(element_id, keys)
 end
 
 def getExpectedValueForPlatform(platform_values)
-  if !defined?($driver) || $driver.nil?
-    fail("This step should only be used if the AppAutomateDriver is present")
+  if !defined?($driver) || MazeRunner.driver.nil?
+    raise('This step should only be used if the AppAutomateDriver is present')
   end
-  os = $driver.capabilities['os']
+
+  os = MazeRunner.driver.capabilities['os']
   expected_value = Hash[platform_values.raw][os]
-  fail("There is no expected value for the current platform \"#{os}\"") if expected_value.nil?
+  raise("There is no expected value for the current platform \"#{os}\"") if expected_value.nil?
+
   expected_value
 end
 
 def shouldSkipPlatformCheck(expected_value)
-  expected_value.eql?("@skip")
+  expected_value.eql?('@skip')
 end
 
 def testStringPlatformValues(field_path, platform_values)
@@ -157,17 +159,17 @@ end
 
 def testBooleanPlatformValues(field_path, platform_values)
   expected_value = getExpectedValueForPlatform(platform_values)
-  unless shouldSkipPlatformCheck(expected_value)
-    if expected_value.downcase == 'true'
-      expected_bool = true
-    elsif expected_value.downcase == 'false'
-      expected_bool = false
-    else
-      expected_bool = expected_value
-    end
-    payload_value = read_key_path(Server.current_request[:body], field_path)
-    assert_equal(expected_bool, payload_value)
-  end
+  return if shouldSkipPlatformCheck(expected_value)
+
+  expected_bool = if expected_value.downcase == 'true'
+                    true
+                  elsif expected_value.downcase == 'false'
+                    false
+                  else
+                    expected_value
+                  end
+  payload_value = read_key_path(Server.current_request[:body], field_path)
+  assert_equal(expected_bool, payload_value)
 end
 
 def testNumericPlatformValues(field_path, platform_values)

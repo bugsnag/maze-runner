28c3cac866679b56f5a57fedadbcd9e9642ae5da
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-29 13:59:21 +0100

    Use method_missing approach

--- lib/features/support/resilient_driver.rb
+++ lib/features/support/resilient_driver.rb
@@ -3,94 +3,51 @@ require_relative './fast_selenium'
 require_relative './logger'
 
 # Handles Appium driver restarts and retries in the case of failure
-class ResilientAppiumDriver < AppAutomateDriver
-  
-  # Checks for an element, waiting until it is present or the method times out
+class ResilientAppiumDriver
+  # Creates the ResilientAppiumDriver
   #
-  # @param element_id [String] the element to search for using the @element_locator strategy
-  # @param timeout [Integer] the maximum time to wait for an element to be present in seconds
-  # @param retry_if_stale [Boolean] enables the method to retry acquiring the element if a StaleObjectException occurs
-  def wait_for_element(element_id, timeout = 15, retry_if_stale = true)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { wait_for_element element_id, timeout, retry_if_stale }
+  # @param username [String] the BrowserStack username
+  # @param access_key [String] the BrowserStack access key
+  # @param local_id [String] the identifier for the BrowserStackLocal tunnel
+  # @param target_device [String] a key from the Devices array selecting which device capabilities to target
+  # @param app_location [String] the location of the test-app to upload
+  # @param locator [Symbol] the primary locator strategy Appium should use to find elements
+  # @param additional_capabilities [Hash] a hash of additional capabilities to be used in this test run
+  def initialize(username, access_key, local_id, target_device, app_location, locator = :id, additional_capabilities = {})
+    @driver = AppAutomateDriver.new username,
+                                    access_key,
+                                    local_id,
+                                    target_device,
+                                    app_location,
+                                    locator,
+                                    additional_capabilities
   end
 
-  # Resets the app
-  def reset
-    $logger.info 'Resetting app'
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { reset }
+  def respond_to_missing?(method_name, include_private = false)
+    @driver.respond_to_missing? method_name
   end
 
-  # Clicks a given element
-  #
-  # @param element_id [String] the element to click using the @element_locator strategy
-  def click_element(element_id)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { click_element element_id }
+  # rubocop: disable MethodMissingSuper
+  def method_missing(method, *args, &block)
+    raise "Method '#{method}' does not exist" unless @driver.respond_to?(method)
+
+    retries = 0
+    until retries >= 5
+      begin
+        @driver.send(method, *args, &block)
+        return
+      rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError => error
+        retries += 1
+        recover
+      end
+    end
+
+    # Re-raise the last error, although it might be better to re-raise the
+    # first error instead.  Review based on whther we ever hit this.
+    raise error unless error.nil?
   end
 
-  # Clears a given element
-  #
-  # @param element_id [String] the element to clear, found using the @element_locator strategy
-  def clear_element(element_id)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { clear_element element_id }
-  end
-
-  # Sends keys to a given element
-  #
-  # @param element_id [String] the element to send text to using the @element_locator strategy
-  # @param text [String] the text to send
-  def send_keys_to_element(element_id, text)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { send_keys_to_element element_id, text }
-  end
-
-  # Sends keys to a given element, clearing it first
-  #
-  # @param element_id [String] the element to clear and send text to using the @element_locator strategy
-  # @param text [String] the text to send
-  def clear_and_send_keys_to_element(element_id, text)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { clear_and_send_keys_to_element element_id, text }
-  end
-
-  # Reset the currently running application after a given timeout
-  #
-  # @param timeout [Number] the amount of time in seconds to wait before resetting
-  def reset_with_timeout(timeout=0.1)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { reset_with_timeout timeout }
-  end
-
-  # Wraps @see Appium::Device#background_app
-  def background_app(timeout)
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { background_app timeout }
-  end
-
-  # Wraps @see Appium::Device#close_app
-  def close_app
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { close_app }
-  end
-
-  # Wraps @see Appium::Device#launch_app
-  def launch_app
-    super
-  rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::WebDriverError
-    recover { launch_app }
-  end
+  private
 
   # Restarts the underlying driver and calls the block given.
   def recover

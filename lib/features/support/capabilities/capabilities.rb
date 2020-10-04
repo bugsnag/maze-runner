fe5faddde16ad7409866e4f2845d7bc7df7e526b
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-10-04 21:01:37 +0100

    String keys please

--- lib/features/support/capabilities/capabilities.rb
+++ lib/features/support/capabilities/capabilities.rb
@@ -10,27 +10,28 @@ class Capabilities
         unless Devices::DEVICE_HASH.keys.include? device_type
 
       capabilities = {
-        'browserstack.console': 'errors',
-        'browserstack.localIdentifier': 'local_id',
-        'browserstack.local': 'true',
-        'browserstack.networkLogs': 'true'
+        'browserstack.console' => 'errors',
+        'browserstack.localIdentifier' => 'local_id',
+        'browserstack.local' => 'true',
+        'browserstack.networkLogs' => 'true'
       }
       capabilities.merge! Devices::DEVICE_HASH[device_type]
     end
 
+    # noinspection RubyStringKeysInHashInspection
     def for_local(platform)
       capabilities = if platform.downcase == 'android'
                        {
-                         'platformName': 'Android',
-                         'automationName': 'UiAutomator2',
+                         'platformName' => 'Android',
+                         'automationName' => 'UiAutomator2',
                          'autoGrantPermissions' => 'true',
-                         'os': 'android'
+                         'os' => 'android'
                        }
                      elsif platform.downcase == 'ios'
                        {
-                         'platformName': 'iOS',
-                         'automationName': 'XCUITest',
-                         'os': 'ios'
+                         'platformName' => 'iOS',
+                         'automationName' => 'XCUITest',
+                         'os' => 'ios'
                        }
                      end
       common = {

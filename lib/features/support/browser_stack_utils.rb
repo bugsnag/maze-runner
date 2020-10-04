12281f01b30403af7941f3ae76f33a37cbdd4a23
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-10-04 15:24:05 +0100

    File upload and tidying

--- lib/features/support/browser_stack_utils.rb
+++ lib/features/support/browser_stack_utils.rb
@@ -8,15 +8,27 @@ class BrowserStackUtils
     # @param username [String] the BrowserStack username
     # @param access_key [String] the BrowserStack access key
     def upload_app(username, access_key, app_location)
-      url = 'https://api-cloud.browserstack.com/app-automate/upload'
-      res = `curl -u "#{username}:#{access_key}" -X POST "#{url}" -F "file=@#{app_location}"`
-      puts res
-      response = JSON.parse(res)
-      raise "BrowserStack upload failed due to error: #{response['error']}" if response.include?('error')
+      # TODO: Improve error handling:
+      #   - res may not be JSON at all
+      #   - what if app_location doesn't exist locally
+      if app_location.start_with? 'bs://'
+        app_url = app_location
+        $logger.info "Skipping upload for pre-uploaded app #{app_location}"
+      else
+        puts "Uploading..."
+        puts username
+        puts access_key
+        puts app_location
+        url = 'https://api-cloud.browserstack.com/app-automate/upload'
+        res = `curl -u "#{username}:#{access_key}" -X POST "#{url}" -F "file=@#{app_location}"`
+        puts res
+        response = JSON.parse(res)
+        raise "BrowserStack upload failed due to error: #{response['error']}" if response.include?('error')
 
-      app_url = response['app_url']
-      $logger.info "app uploaded to: #{app_url}"
-      $logger.info 'You can use this url to avoid uploading the same app more than once.'
+        app_url = response['app_url']
+        $logger.info "app uploaded to: #{app_url}"
+        $logger.info 'You can use this url to avoid uploading the same app more than once.'
+      end
       app_url
     end
 

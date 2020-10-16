9bdbedbbdd3bebc70c47557e52c62ea911826862
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-10-16 21:12:32 +0100

    Remove commented code

--- test/appium_driver_test.rb
+++ test/appium_driver_test.rb
@@ -218,30 +218,6 @@ class AppiumDriverTest < Test::Unit::TestCase
     driver.clear_and_send_keys_to_element('test_text_entry', 'Test_text')
   end
 
-  # TODO Tests will follow in a subsequent PR after properly reassessing the logging
-  # def test_environment_ids
-  #   ENV['BRANCH_NAME'] = 'TEST BRANCH'
-  #   ENV['BUILDKITE'] = 'true'
-  #   ENV['BUILDKITE_PIPELINE_NAME'] = 'TEST'
-  #   ENV['BUILDKITE_BUILD_NUMBER'] = '156'
-  #   ENV['BUILDKITE_RETRY_COUNT'] = '5'
-  #   ENV['BUILDKITE_STEP_KEY'] = 'tests-05'
-  #   logger_mock = mock('logger')
-  #   $logger = logger_mock
-  #   logger_mock.expects(:info).with("app uploaded to: #{TEST_APP_URL}").once
-  #   logger_mock.expects(:info).with('You can use this url to avoid uploading the same app more than once.').once
-  #   logger_mock.expects(:info).with('Appium driver initialised for:').once
-  #   logger_mock.expects(:info).with('    project : TEST').once
-  #   logger_mock.expects(:info).with('    build   : 156 TEST BRANCH')
-  #   logger_mock.expects(:info).with("    name    : #{TARGET_DEVICE} tests-05 5")
-  #
-  #   driver = AppiumDriver.new SERVER_URL, @capabilities
-  #
-  #   assert_equal('TEST', driver.caps[:project])
-  #   assert_equal('156 TEST BRANCH', driver.caps[:build])
-  #   assert_equal("#{TARGET_DEVICE} tests-05 5", driver.caps[:name])
-  # end
-
   def test_project_name_capabilities_local
     # BUILDKITE environment variable is cleared in setup
     start_logger_mock

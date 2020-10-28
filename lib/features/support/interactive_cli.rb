2270b28bd8fd268fa6610f2ca7035531961567eb
Alex Moinet <alex@bugsnag.com>
2020-10-28 12:01:54 +0000

    Add Interactive CLI: Address feedback regarding naming and comments

--- lib/features/support/interactive_cli.rb
+++ lib/features/support/interactive_cli.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'open3'
 
-# Encapsulates a terminal session, retaining state and input streams for interactive tests
+# Encapsulates a shell session, retaining state and input streams for interactive tests
 class InteractiveCLI
-  # @!attribute [r] parsed_output
+  # @!attribute [r] stdout_lines
   #   @return [Array] An array of output strings received from the terminals STDOUT pipe
-  attr_reader :parsed_output
+  attr_reader :stdout_lines
 
-  # @!attribute [r] parsed_error
+  # @!attribute [r] stderr_lines
   #   @return [Array] An array of error strings received from the terminals STDERR pipe
-  attr_reader :parsed_errors
+  attr_reader :stderr_lines
 
   # @!attribute [r] last_exit_code
-  #   @return [Number] The exit code of the last terminal command
+  #   @return [Number, nil] The exit code of the last terminal command
   attr_reader :last_exit_code
 
   # @!attribute [r] pid
-  #   @return [Number] The PID of the running terminal
+  #   @return [Number, nil] The PID of the running terminal
   attr_reader :pid
 
   # @!attribute [r] current_buffer
@@ -25,40 +27,45 @@ class InteractiveCLI
   # Creates an InteractiveCLI instance
   #
   # @param environment [Hash] A hash of environment variables
-  # @param terminal [String] A path to the terminal to run, defaults to `/bin/sh`
-  # @param terminal [String] The stop command, defaults to `exit`
-  def initialize(environment = {}, terminal = '/bin/sh', stop_command = 'exit')
+  # @param shell [String] A path to the shell to run, defaults to `/bin/sh`
+  # @param stop_command [String] The stop command, defaults to `exit`
+  def initialize(environment = {}, shell = '/bin/sh', stop_command = 'exit')
+    @shell = shell
     @env = environment
-    @parsed_output = []
-    @parsed_errors = []
+    @stdout_lines = []
+    @stderr_lines = []
     @current_buffer = ''
     @last_exit_code = nil
     @in_stream = nil
     @pid = nil
     @running = false
     @stop_command = stop_command
-    start_threaded_terminal(terminal)
+    start_threaded_shell(shell)
   end
 
-  # Attempts to stop the terminal using the preset command
+  def start(threaded = true)
+    threaded ? start_threaded_shell(@shell) : start_shell(@shell)
+  end
+
+  # Attempts to stop the shell using the preset command
   #
-  # @return [Boolean] true if successful, false otherwise
+  # @return [Boolean] true if the command is executed, false otherwise
   def stop
     run_command(@stop_command)
   end
 
-  # Returns whether the terminal is running by verifying the in_stream exists and is open
+  # Returns whether the shell is running by verifying the in_stream exists and is open
   #
   # @return [Boolean] Whether the stream is currently running
   def running
     !(@in_stream.nil? || @in_stream.closed?)
   end
 
-  # Runs the given command if the terminal is running
+  # Runs the given command if the shell is running
   #
   # @param command [String] The command to run
   #
-  # @return [Boolean] true if successful, false otherwise
+  # @return [Boolean] true if the command is executed, false otherwise
   def run_command(command)
     return false unless running
     @in_stream.puts(command)
@@ -67,31 +74,30 @@ class InteractiveCLI
 
   private
 
-  # Starts a terminal on another thread
+  # Starts a shell on another thread
   #
-  # @param terminal [String] A path to the terminal to run
-  def start_threaded_terminal(terminal)
+  # @param shell [String] A path to the shell to run
+  def start_threaded_shell(shell)
     executor = lambda do
-      start_terminal(terminal)
+      start_shell(shell)
     end
 
-    Thread.new &executor
+    @thread = Thread.new &executor
   end
 
-  # Starts a terminal
+  # Starts a shell
   #
-  # @param terminal [String] A path to the terminal to run
-  def start_terminal(terminal)
-    Open3.popen3(@env, terminal) do |stdin, stdout, stderr, wait_thr|
+  # @param shell [String] A path to the shell to run
+  def start_shell(shell)
+    Open3.popen3(@env, shell) do |stdin, stdout, stderr, wait_thr|
       @pid = wait_thr.pid
-      $logger.debug(pid) { 'Starting terminal session' }
+      $logger.debug(pid) { "Starting shell session: #{shell}" }
 
       @in_stream = stdin
-      @out_stream = stdout
 
       stdout.each_char do |char|
         if char == "\n"
-          @parsed_output << parse_terminal_line(@current_buffer)
+          @stdout_lines << format_line(@current_buffer)
           $logger.debug(pid) { current_buffer }
           @current_buffer.clear
         else
@@ -100,7 +106,7 @@ class InteractiveCLI
       end
 
       stderr.each do |line|
-        @parsed_errors << parse_terminal_line(line)
+        @stderr_lines << format_line(line)
         $logger.debug(pid) { line }
       end
 
@@ -110,8 +116,8 @@ class InteractiveCLI
     end
   end
 
-  # Strips whitespace from terminal lines, can be used to sanitize further if required
-  def parse_terminal_line(line)
+  # Strips whitespace from shell lines, can be used to sanitize further if required
+  def format_line(line)
     line.strip
   end
 end

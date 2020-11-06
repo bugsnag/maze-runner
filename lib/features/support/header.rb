ba18ef6e57200336d0122db83c2b662a49e0efe7
fractalwrench <fractalwrench@gmail.com>
2020-11-06 16:24:47 +0000

    address review feedback

--- lib/features/support/header.rb
+++ lib/features/support/header.rb
@@ -2,8 +2,15 @@ require 'digest/sha1'
 require 'json'
 
 def valid_bugsnag_integrity_header(request)
-  valid = valid_sha_digest_header(request) || valid_simple_digest_header(request)
-  assert_true(valid, "Bugsnag-Integrity header does not match SHA1 or Request Length digest.")
+  header = request[:request]["Bugsnag-Integrity"]
+
+  if header.start_with?("sha1")
+    assert_true(valid_sha_digest_header(request) , "Bugsnag-Integrity header does not match SHA1 digest.")
+  elsif header.start_with?("simple")
+    assert_true(valid_simple_digest_header(request) , "Bugsnag-Integrity header does not match simple digest.")
+  else
+    assert_true(false, "Bugsnag-Integrity header does not match SHA1 or Request Length digest.")
+  end
 end
 
 def valid_sha_digest_header(request)
@@ -11,7 +18,7 @@ def valid_sha_digest_header(request)
   assert_not_nil(request[:body], "No request body present")
   body = JSON.generate(request[:body])
   computed_digest = "sha1 " + Digest::SHA1.hexdigest(body.to_s)
-  assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest} from #{body}.")
+  assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest}.")
   return header == computed_digest
 end
 
@@ -20,6 +27,6 @@ def valid_simple_digest_header(request)
   assert_not_nil(request[:body], "No request body present")
   body = JSON.generate(request[:body])
   computed_digest = "simple #{body.to_s.size}"
-  assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest} from #{body}.")
+  assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest}.")
   return header == computed_digest
 end

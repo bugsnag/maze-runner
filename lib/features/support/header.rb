3f500f82760f1ba67d58c9a2bb59e2d739966885
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-09 23:09:51 +0000

    Style corrections

--- lib/features/support/header.rb
+++ lib/features/support/header.rb
@@ -2,31 +2,31 @@ require 'digest/sha1'
 require 'json'
 
 def valid_bugsnag_integrity_header(request)
-  header = request[:request]["Bugsnag-Integrity"]
+  header = request[:request]['Bugsnag-Integrity']
 
-  if header.start_with?("sha1")
-    assert_true(valid_sha_digest_header(request) , "Bugsnag-Integrity header does not match SHA1 digest.")
-  elsif header.start_with?("simple")
-    assert_true(valid_simple_digest_header(request) , "Bugsnag-Integrity header does not match simple digest.")
+  if header.start_with?('sha1')
+    assert_true(valid_sha_digest_header(request), 'Bugsnag-Integrity header does not match SHA1 digest.')
+  elsif header.start_with?('simple')
+    assert_true(valid_simple_digest_header(request), 'Bugsnag-Integrity header does not match simple digest.')
   else
-    assert_true(false, "Bugsnag-Integrity header does not match SHA1 or Request Length digest.")
+    assert_true(false, 'Bugsnag-Integrity header does not match SHA1 or Request Length digest.')
   end
 end
 
 def valid_sha_digest_header(request)
-  header = request[:request]["Bugsnag-Integrity"]
-  assert_not_nil(request[:body], "No request body present")
+  header = request[:request]['Bugsnag-Integrity']
+  assert_not_nil(request[:body], 'No request body present')
   body = JSON.generate(request[:body])
-  computed_digest = "sha1 " + Digest::SHA1.hexdigest(body.to_s)
+  computed_digest = 'sha1 ' + Digest::SHA1.hexdigest(body.to_s)
   assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest}.")
-  return header == computed_digest
+  header == computed_digest
 end
 
 def valid_simple_digest_header(request)
-  header = request[:request]["Bugsnag-Integrity"]
-  assert_not_nil(request[:body], "No request body present")
+  header = request[:request]['Bugsnag-Integrity']
+  assert_not_nil(request[:body], 'No request body present')
   body = JSON.generate(request[:body])
   computed_digest = "simple #{body.to_s.size}"
   assert_equal(header, computed_digest, "SHA1 digest does not match request payload. Computed #{computed_digest}.")
-  return header == computed_digest
+  header == computed_digest
 end

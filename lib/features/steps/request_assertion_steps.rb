76688ffd73b4fa9eb54f1bb72b79268c4019e185
fractalwrench <fractalwrench@gmail.com>
2020-11-13 14:08:57 +0000

    docs: reword request mismatch error message

--- lib/features/steps/request_assertion_steps.rb
+++ lib/features/steps/request_assertion_steps.rb
@@ -25,7 +25,8 @@ Then('I wait to receive {int} request(s)') do |request_count|
     received = (Server.stored_requests.size >= request_count)
     sleep 0.1
   end
-  raise "Requests not received in 30s (received #{Server.stored_requests.size})" unless received
+  raise "Expected #{request_count} requests but received #{Server.stored_requests.size} within the 30s timeout. This could indicate that Bugsnag crashed with a fatal error, or that it hasn’t made the requests that it should have done. Please check the device logs to confirm." unless received
+  
   assert_equal(request_count, Server.stored_requests.size, "#{Server.stored_requests.size} requests received")
 end
 

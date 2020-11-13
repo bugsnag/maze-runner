88b6913e55d3e0086d322a9351f66bca28b8e810
Alex Moinet <alex@bugsnag.com>
2020-11-13 08:50:39 +0000

    Add multipart_form_data validation for all requests, fix issues with deprecated steps

--- lib/features/steps/multipart_request_steps.rb
+++ lib/features/steps/multipart_request_steps.rb
@@ -7,12 +7,25 @@ include Test::Unit::Assertions
 
 # @!group Multipart request assertion steps
 
+# Verifies a request contains the correct Content-Type header and some contents
+# for a multipart/form-data request
+#
+# @param request [Hash] The payload to test
+def valid_multipart_form_data?(request)
+  content_regex = Regexp.new('^multipart\\/form-data; boundary=[\\h-]+$')
+  content_header = request[:request]['Content-Type']
+  assert_match(content_regex, content_header)
+  assert(request[:body].size.positive?, "Multipart request payload contained #{request[:body].size} fields")
+end
+
 # Verifies that the request contains multipart form-data
 Then('the request is valid multipart form-data') do
-  steps %(
-    Then the "Content-Type" header matches the regex "^multipart\\/form-data; boundary=[\\h-]+$"
-    And the multipart request has a non-empty body
-  )
+  valid_multipart_form_data?(Server.current_request)
+end
+
+# Verifies all received requests contain multipart form-data
+Then('all requests are valid multipart form-data') do
+  Server.stored_requests.all? { |request| valid_multipart_form_data?(request) }
 end
 
 # Tests the number of fields a multipart request contains.
@@ -36,7 +49,8 @@ end
 #
 # @step_input part_key [String] The key to the multipart element
 Then('the field {string} for multipart request is not null') do |part_key|
-  step %(the multipart field "#{part_key}" is not null)
+  parts = Server.current_request[:body]
+  assert_not_nil(parts[part_key], "The field '#{part_key}' should not be null")
 end
 
 # (Deprecated) Retained for backwards compatibility
@@ -47,7 +61,8 @@ end
 # @step_input part_key [String] The key to the multipart element
 # @step_input expected_value [String] The string to match against
 Then('the field {string} for multipart request equals {string}') do |part_key, expected_value|
-  step %(the multipart field "#{part_key}" equals "#{expected_value}")
+  parts = Server.current_request[:body]
+  assert_equal(parts[part_key], expected_value)
 end
 
 # (Deprecated) Retained for backwards compatibility
@@ -57,7 +72,8 @@ end
 #
 # @step_input part_key [String] The key to the multipart element
 Then('the field {string} for multipart request is null') do |part_key|
-  step %(the multipart field "#{part_key}" is null)
+  parts = Server.current_request[:body]	
+  assert_nil(parts[part_key], "The field '#{part_key}' should be null")
 end
 
 # Takes a hashmap and parses all fields into strings or hashes depending on their format
@@ -79,7 +95,7 @@ end
 #
 # @step_input json_path [String] Path to a JSON file relative to maze-runner root
 Then('the multipart body does not match the JSON file in {string}') do |json_path|
-  assert_true(File.exist?(json_path, "'#{json_path}' does not exist"))
+  assert_true(File.exist?(json_path), "'#{json_path}' does not exist")
   raw_payload_value = Server.current_request[:body]
   payload_value = parse_multipart_body(raw_payload_value)
   expected_value = JSON.parse(open(json_path, &:read))
@@ -92,7 +108,7 @@ end
 #
 # @step_input json_path [String] Path to a JSON file relative to maze-runner root
 Then('the multipart body matches the JSON file in {string}') do |json_path|
-  assert_true(File.exist?(json_path, "'#{json_path}' does not exist"))
+  assert_true(File.exist?(json_path), "'#{json_path}' does not exist")
   raw_payload_value = Server.current_request[:body]
   payload_value = parse_multipart_body(raw_payload_value)
   expected_value = JSON.parse(open(json_path, &:read))
@@ -106,7 +122,7 @@ end
 # @step_input field_path [String] Path to the tested element
 # @step_input json_path [String] Path to a JSON file relative to maze-runner root
 Then('the multipart field {string} matches the JSON file in {string}') do |field_path, json_path|
-  assert_true(File.exist?(json_path, "'#{json_path}' does not exist"))
+  assert_true(File.exist?(json_path), "'#{json_path}' does not exist")
   payload_value = JSON.parse(Server.current_request[:body][field_path].to_s)
   expected_value = JSON.parse(open(json_path, &:read))
   result = value_compare(expected_value, payload_value)
--- test/fixtures/payload-helpers/features/multipart_request.feature
+++ test/fixtures/payload-helpers/features/multipart_request.feature
@@ -23,3 +23,8 @@ Feature: Testing multipart requests work correctly
         And the multipart field "json_file" matches the JSON file in "features/fixtures/file.json"
         And the multipart body matches the JSON file in "features/fixtures/full_multipart.json"
         And the multipart body does not match the JSON file in "features/fixtures/breadcrumb_match.json"
+
+    Scenario: Multiple multipart payloads can be validated at once
+        When I run the script "features/scripts/send_multi_multipart.sh" synchronously
+        Then I wait to receive 3 requests
+        And all requests are valid multipart form-data

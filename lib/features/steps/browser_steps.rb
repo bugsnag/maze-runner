3b6a2bf4057bab3f0154509debf9912909d6a277
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-02 22:18:24 +0000

    BROWSER envier no longer in use

--- lib/features/steps/browser_steps.rb
+++ lib/features/steps/browser_steps.rb
@@ -28,7 +28,7 @@ When('I let the test page run for up to {int} seconds') do |n|
 end
 
 Then(/^the request is a valid browser payload for the error reporting API$/) do
-  if !/^ie_(8|9|10)$/.match(ENV['BROWSER'])
+  if !/^ie_(8|9|10)$/.match(MazeRunner.config.bs_browser)
     steps %(
       Then the error "Bugsnag-API-Key" header is not null
       And the error "Content-Type" header equals one of:
@@ -60,7 +60,7 @@ Then(/^the request is a valid browser payload for the error reporting API$/) do
 end
 
 Then('the request is a valid browser payload for the session tracking API') do
-  if !/^ie_(8|9|10)$/.match(ENV['BROWSER'])
+  if !/^ie_(8|9|10)$/.match(MazeRunner.config.bs_browser)
     steps %(
       Then the session "Bugsnag-API-Key" header is not null
       And the session "Content-Type" header equals one of:

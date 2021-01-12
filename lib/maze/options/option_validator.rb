f57ababc852c2940e18f5620d3bf4d3c85a746dc
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-12 21:19:07 +0000

    Remove redundant qualifiers

--- lib/maze/options/option_validator.rb
+++ lib/maze/options/option_validator.rb
@@ -16,13 +16,13 @@ module Maze
         # Common options
         farm = options[Option::FARM]
         if farm && !%w[bs local].include?(farm)
-          errors << "--#{Maze::Option::FARM} must be either 'bs' or 'local' if provided"
+          errors << "--#{Option::FARM} must be either 'bs' or 'local' if provided"
         end
 
         begin
           JSON.parse(options[Option::CAPABILITIES])
         rescue JSON::ParserError
-          errors << "--#{Maze::Option::CAPABILITIES} must be valid JSON (given #{options[Option::CAPABILITIES]})"
+          errors << "--#{Option::CAPABILITIES} must be valid JSON (given #{options[Option::CAPABILITIES]})"
         end
 
         # Farm specific options
@@ -35,14 +35,14 @@ module Maze
       # Validates BrowserStack options
       def validate_bs(options, errors)
         # BS local binary
-        bs_local = options[Maze::Option::BS_LOCAL]
+        bs_local = options[Option::BS_LOCAL]
         errors << "BrowserStack local binary '#{bs_local}' not found" unless File.exist? bs_local
 
         # Device
-        bs_browser = options[Maze::Option::BS_BROWSER]
-        bs_device = options[Maze::Option::BS_DEVICE]
+        bs_browser = options[Option::BS_BROWSER]
+        bs_device = options[Option::BS_DEVICE]
         if bs_browser.nil? && bs_device.nil?
-          errors << "Either --#{Maze::Option::BS_BROWSER} or --#{Maze::Option::BS_DEVICE} must be specified"
+          errors << "Either --#{Option::BS_BROWSER} or --#{Option::BS_DEVICE} must be specified"
         elsif bs_browser
 
           browsers = YAML.safe_load(File.read("#{__dir__}/../features/support/capabilities/browsers.yml"))
@@ -56,45 +56,45 @@ module Maze
             errors << "Device type '#{bs_device}' unknown on BrowserStack.  Must be one of #{Devices::DEVICE_HASH.keys}"
           end
           # App
-          app = options[Maze::Option::APP]
+          app = options[Option::APP]
           if app.nil?
-            errors << "--#{Maze::Option::APP} must be provided when running on a device"
+            errors << "--#{Option::APP} must be provided when running on a device"
           else
             errors << "app file '#{app}' not found" unless app.start_with?('bs://') || File.exist?(app)
           end
         end
 
         # Credentials
-        errors << "--#{Maze::Option::USERNAME} must be specified" if options[Maze::Option::USERNAME].nil?
-        errors << "--#{Maze::Option::ACCESS_KEY} must be specified" if options[Maze::Option::ACCESS_KEY].nil?
+        errors << "--#{Option::USERNAME} must be specified" if options[Option::USERNAME].nil?
+        errors << "--#{Option::ACCESS_KEY} must be specified" if options[Option::ACCESS_KEY].nil?
       end
 
       # Validates Local device options
       def validate_local(options, errors)
-        errors << "--#{Maze::Option::APP} must be specified" if options[Maze::Option::APP].nil?
+        errors << "--#{Option::APP} must be specified" if options[Option::APP].nil?
 
         # OS
-        if options[Maze::Option::OS].nil?
-          errors << "--#{Maze::Option::OS} must be specified"
+        if options[Option::OS].nil?
+          errors << "--#{Option::OS} must be specified"
         else
-          os = options[Maze::Option::OS].downcase
+          os = options[Option::OS].downcase
           errors << 'os must be android, ios or macos' unless %w[android ios macos].include? os
           if os == 'ios'
-            if options[Maze::Option::APPLE_TEAM_ID].nil?
-              errors << "--#{Maze::Option::APPLE_TEAM_ID} must be specified for iOS"
+            if options[Option::APPLE_TEAM_ID].nil?
+              errors << "--#{Option::APPLE_TEAM_ID} must be specified for iOS"
             end
-            errors << "--#{Maze::Option::UDID} must be specified for iOS" if options[Maze::Option::UDID].nil?
+            errors << "--#{Option::UDID} must be specified for iOS" if options[Option::UDID].nil?
           end
         end
 
         # OS Version
-        if options[Maze::Option::OS_VERSION].nil?
-          errors << "--#{Maze::Option::OS_VERSION} must be specified"
+        if options[Option::OS_VERSION].nil?
+          errors << "--#{Option::OS_VERSION} must be specified"
         else
           # Ensure OS version is a valid float so that notifier tests can perform numeric checks
           # e.g 'Maze.config.os_version > 7'
-          unless /^[1-9][0-9]*(\.[0-9])?/.match? options[Maze::Option::OS_VERSION]
-            errors << "--#{Maze::Option::OS_VERSION} must be a valid version matching '/^[1-9][0-9]*(\\.[0-9])?/'"
+          unless /^[1-9][0-9]*(\.[0-9])?/.match? options[Option::OS_VERSION]
+            errors << "--#{Option::OS_VERSION} must be a valid version matching '/^[1-9][0-9]*(\\.[0-9])?/'"
           end
         end
       end

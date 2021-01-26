bb28c388745539edcc0890887f54afead90a641f
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-26 16:23:27 +0000

    Improve readability of condition.
    Co-authored-by: Joe Haines <hello@joehaines.co.uk>

--- lib/maze/request_list.rb
+++ lib/maze/request_list.rb
@@ -72,7 +72,7 @@ module Maze
       header = 'Bugsnag-Sent-At'
       sub_list = @requests[@current...@current + count]
 
-      return unless sub_list.all? { |r| !r[:request][header].nil? }
+      return if sub_list.any? { |r| r[:request][header].nil? }
 
       # Sort sublist by Bugsnag-Sent-At and overwrite in the main list
       sub_list.sort_by! { |r| DateTime.parse(r[:request][header]) }

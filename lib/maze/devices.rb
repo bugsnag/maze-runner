7d7cf7008bdc0649043f8f11b1e7f96e6ab18f0e
Steve Kirkland <steve@bugsnag.com>
2021-03-27 19:02:04 +0000

    Default to Appium 1.20.2

--- lib/maze/devices.rb
+++ lib/maze/devices.rb
@@ -4,14 +4,12 @@ module Maze
   # Provides a source of capabilities used to run tests against specific BrowserStack devices
   # noinspection RubyStringKeysInHashInspection
   class Devices
-    APPIUM_1_9_1 = '1.9.1' # 4
-    APPIUM_1_15_0 = '1.15.0' # 6
-    APPIUM_1_17_0 = '1.17.0' # 6
-    APPIUM_1_18_0 = '1.18.0' # 7
-    APPIUM_1_20_2 = '1.20.2' # 8 +
+    APPIUM_1_9_1 = '1.9.1'
+    APPIUM_1_15_0 = '1.15.0'
+    APPIUM_1_20_2 = '1.20.2'
 
     class << self
-      def make_android_hash(device, version, appium_version)
+      def make_android_hash(device, version, appium_version = APPIUM_1_20_2)
         {
           'device' => device,
           'os_version' => version,
@@ -22,7 +20,7 @@ module Maze
         }.freeze
       end
 
-      def add_android(device, version, appium_version, hash)
+      def add_android(device, version, hash, appium_version = APPIUM_1_20_2)
         # Key format is "ANDROID_<version>_<device>", with:
         # - dots in versions and all spaces replaced with underscores
         # - device made upper case
@@ -69,14 +67,14 @@ module Maze
       def create_hash
         hash = {
           # Classic, non-specific devices for each Android version
-          'ANDROID_11_0' => make_android_hash('Google Pixel 4', '11.0', APPIUM_1_17_0),
-          'ANDROID_10_0' => make_android_hash('Google Pixel 4', '10.0', APPIUM_1_17_0),
-          'ANDROID_9_0' => make_android_hash('Google Pixel 3', '9.0', APPIUM_1_17_0),
-          'ANDROID_8_1' => make_android_hash('Samsung Galaxy Note 9', '8.1', APPIUM_1_17_0),
-          'ANDROID_8_0' => make_android_hash('Google Pixel 2', '8.0', APPIUM_1_17_0),
-          'ANDROID_7_1' => make_android_hash('Google Pixel', '7.1', APPIUM_1_17_0),
-          'ANDROID_6_0' => make_android_hash('Google Nexus 6', '6.0', APPIUM_1_17_0),
-          'ANDROID_5_0' => make_android_hash('Google Nexus 6', '5.0', APPIUM_1_17_0),
+          'ANDROID_11_0' => make_android_hash('Google Pixel 4', '11.0'),
+          'ANDROID_10_0' => make_android_hash('Google Pixel 4', '10.0'),
+          'ANDROID_9_0' => make_android_hash('Google Pixel 3', '9.0'),
+          'ANDROID_8_1' => make_android_hash('Samsung Galaxy Note 9', '8.1'),
+          'ANDROID_8_0' => make_android_hash('Google Pixel 2', '8.0'),
+          'ANDROID_7_1' => make_android_hash('Google Pixel', '7.1'),
+          'ANDROID_6_0' => make_android_hash('Google Nexus 6', '6.0'),
+          'ANDROID_5_0' => make_android_hash('Google Nexus 6', '5.0'),
           'ANDROID_4_4' => make_android_hash('Google Nexus 5', '4.4', APPIUM_1_9_1),
 
           # iOS devices
@@ -95,22 +93,22 @@ module Maze
         hash['ANDROID_4'] = hash['ANDROID_4_4']
 
         # Specific Android devices
-        add_android 'Google Pixel 4', '11.0', APPIUM_1_17_0, hash          # ANDROID_11_0_GOOGLE_PIXEL_4
-        add_android 'Samsung Galaxy Note 9', '8.1', APPIUM_1_17_0, hash    # ANDROID_8_1_SAMSUNG_GALAXY_NOTE_9
-        add_android 'Samsung Galaxy J7 Prime', '8.1', APPIUM_1_17_0, hash  # ANDROID_8_1_SAMSUNG_GALAXY_J7_PRIME
-        add_android 'Samsung Galaxy Tab S4', '8.1', APPIUM_1_17_0, hash    # ANDROID_8_1_SAMSUNG_GALAXY_TAB_S4
-        add_android 'Google Pixel', '8.0', APPIUM_1_17_0, hash             # ANDROID_8_0_GOOGLE_PIXEL
-        add_android 'Google Pixel 2', '8.0', APPIUM_1_17_0, hash           # ANDROID_8_0_GOOGLE_PIXEL_2
-        add_android 'Samsung Galaxy S9', '8.0', APPIUM_1_17_0, hash        # ANDROID_8_0_SAMSUNG_GALAXY_S9
-        add_android 'Samsung Galaxy S9 Plus', '8.0', APPIUM_1_17_0, hash   # ANDROID_8_0_SAMSUNG_GALAXY_S9_PLUS
-        add_android 'Samsung Galaxy Tab S3', '7.0', APPIUM_1_17_0, hash    # ANDROID_7_0_SAMSUNG_GALAXY_TAB_S3
-        add_android 'Motorola Moto X 2nd Gen', '6.0', APPIUM_1_17_0, hash  # ANDROID_6_0_MOTOROLA_MOTO_X_2ND_GEN
-        add_android 'Google Nexus 6', '6.0', APPIUM_1_17_0, hash           # ANDROID_6_0_GOOGLE_NEXUS_6
-        add_android 'Samsung Galaxy S7', '6.0', APPIUM_1_17_0, hash        # ANDROID_6_0_SAMSUNG_GALAXY_S7
-        add_android 'Google Nexus 6', '5.0', APPIUM_1_17_0, hash           # ANDROID_5_0_GOOGLE_NEXUS_6
-        add_android 'Samsung Galaxy S6', '5.0', APPIUM_1_17_0, hash        # ANDROID_5_0_SAMSUNG_GALAXY_S6
-        add_android 'Samsung Galaxy Note 4', '4.4', APPIUM_1_9_1, hash     # ANDROID_4_4_SAMSUNG_GALAXY_NOTE_4
-        add_android 'Samsung Galaxy Tab 4', '4.4', APPIUM_1_9_1, hash      # ANDROID_4_4_SAMSUNG_GALAXY_TAB_4
+        add_android 'Google Pixel 4', '11.0', hash                        # ANDROID_11_0_GOOGLE_PIXEL_4
+        add_android 'Samsung Galaxy Note 9', '8.1', hash                  # ANDROID_8_1_SAMSUNG_GALAXY_NOTE_9
+        add_android 'Samsung Galaxy J7 Prime', '8.1', hash                # ANDROID_8_1_SAMSUNG_GALAXY_J7_PRIME
+        add_android 'Samsung Galaxy Tab S4', '8.1', hash                  # ANDROID_8_1_SAMSUNG_GALAXY_TAB_S4
+        add_android 'Google Pixel', '8.0', hash                           # ANDROID_8_0_GOOGLE_PIXEL
+        add_android 'Google Pixel 2', '8.0', hash                         # ANDROID_8_0_GOOGLE_PIXEL_2
+        add_android 'Samsung Galaxy S9', '8.0', hash                      # ANDROID_8_0_SAMSUNG_GALAXY_S9
+        add_android 'Samsung Galaxy S9 Plus', '8.0', hash                 # ANDROID_8_0_SAMSUNG_GALAXY_S9_PLUS
+        add_android 'Samsung Galaxy Tab S3', '7.0', hash                  # ANDROID_7_0_SAMSUNG_GALAXY_TAB_S3
+        add_android 'Motorola Moto X 2nd Gen', '6.0', hash                # ANDROID_6_0_MOTOROLA_MOTO_X_2ND_GEN
+        add_android 'Google Nexus 6', '6.0', hash                         # ANDROID_6_0_GOOGLE_NEXUS_6
+        add_android 'Samsung Galaxy S7', '6.0', hash                      # ANDROID_6_0_SAMSUNG_GALAXY_S7
+        add_android 'Google Nexus 6', '5.0', hash                         # ANDROID_5_0_GOOGLE_NEXUS_6
+        add_android 'Samsung Galaxy S6', '5.0', hash                      # ANDROID_5_0_SAMSUNG_GALAXY_S6
+        add_android 'Samsung Galaxy Note 4', '4.4', hash, APPIUM_1_9_1    # ANDROID_4_4_SAMSUNG_GALAXY_NOTE_4
+        add_android 'Samsung Galaxy Tab 4', '4.4', hash, APPIUM_1_9_1     # ANDROID_4_4_SAMSUNG_GALAXY_TAB_4
 
         # Specific iOS devices
         add_ios 'iPhone 8 Plus', '11.0', hash                             # IOS_11_0_IPHONE_8_PLUS

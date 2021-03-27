330630014ef9a6be25a3ee6c53169863282ea2bc
Steve Kirkland <steve@bugsnag.com>
2021-03-27 15:08:15 +0000

    noReset when running locally

--- lib/maze/capabilities.rb
+++ lib/maze/capabilities.rb
@@ -45,7 +45,9 @@ module Maze
                          {
                            'platformName' => 'Android',
                            'automationName' => 'UiAutomator2',
-                           'autoGrantPermissions' => 'true'
+                           'autoGrantPermissions' => 'true',
+                           # 'skipDeviceInitialization' => 'true',
+                           'noReset' => 'true'
                          }
                        elsif platform.downcase == 'ios'
                          {

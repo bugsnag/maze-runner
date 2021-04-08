79e2c3c7cbdce4e56aee181a5f23deaf98396170
Steve Kirkland <steve@bugsnag.com>
2021-04-08 08:59:32 +0100

    Add license finder to pipeline

---
+++ scripts/license_finder.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/global.yml -o config/global.yml
+bundle install
+bundle exec license_finder --decisions-file=config/global.yml

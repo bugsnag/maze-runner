a91143a1722dd231d83667f03536aed97a983ece
Steve Kirkland <steve@bugsnag.com>
2021-04-12 22:06:32 +0100

    Use license_finder image

--- scripts/license_finder.sh
+++ scripts/license_finder.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/global.yml -o config/global.yml
 bundle install
-bundle exec license_finder --decisions-file=config/global.yml
+license_finder --decisions-file=config/global.yml

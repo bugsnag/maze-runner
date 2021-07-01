189ab1afd805bbfbf31661e8642f2a9feed75acf
Alex Moinet <alex@bugsnag.com>
2021-07-01 09:12:19 +0100

    BitBar/File-upload: Ensure terms such as id are consistent, remove unnecessary log

--- lib/maze/bitbar_utils.rb
+++ lib/maze/bitbar_utils.rb
@@ -10,11 +10,10 @@ module Maze
       # @param api_key [String] The BitBar API key
       # @param app [String] A path to the application file
       def upload_app(api_key, app)
-        pp app
         uuid_regex = /\A[0-9]{8,12}\z/
 
         if uuid_regex.match? app
-          $logger.info "Using pre-uploaded app with UUID #{app}"
+          $logger.info "Using pre-uploaded app with ID #{app}"
           app_uuid = app
         else
           expanded_app = Maze::Helper.expand_path(app)
@@ -33,17 +32,16 @@ module Maze
           # Pull the UUID from the response
           begin
             response = JSON.parse res.body
-
             if response.key?('id')
               app_uuid = response['id']
-              $logger.info "Uploaded app UUID: #{app_uuid}"
-              $logger.info 'You can use this UUID to avoid uploading the same app more than once.'
+              $logger.info "Uploaded app ID: #{app_uuid}"
+              $logger.info 'You can use this ID to avoid uploading the same app more than once.'
             else
               $logger.error "Unexpected response body: #{response}"
               raise 'App upload failed'
             end
           rescue JSON::ParserError
-            $logger.error "Expected JSON response, received: #{body}"
+            $logger.error "Expected JSON response, received: #{res.body}"
             raise
           end
         end

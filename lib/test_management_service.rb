2c0ea89e0dc0750d21623a8c58bd2cab476d6f1d
Alex Moinet <alex@bugsnag.com>
2021-08-24 17:30:30 +0100

    BitBar/TMS: Fix issues with JSON parsing cached sessions

--- lib/test_management_service.rb
+++ lib/test_management_service.rb
@@ -3,10 +3,12 @@
 require_relative 'test_management_service/bitbar_account_manager'
 require_relative 'test_management_service/server'
 
-server = WEBrick::HTTPServer.new(:Port => 1234)
+ACCOUNT_MAXIMUM = ENV['BITBAR_ACCOUNT_MAX'].to_i
+
+server = WEBrick::HTTPServer.new(:Port => 9340)
 
 server.mount "/account",
              TestManagementService::AccountServlet,
-             TestManagementService.BitbarAccountManager.new(10)
+             TestManagementService::BitbarAccountManager.new(ACCOUNT_MAXIMUM)
 
 server.start

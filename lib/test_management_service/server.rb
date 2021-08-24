bba57be6a1a5dff5b4e6a427466d0a1fc6d209a9
Alex Moinet <alex@bugsnag.com>
2021-08-24 08:53:06 +0100

    BitBar/TMS: Initial commit

---
+++ lib/test_management_service/server.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'webrick'
+require 'cgi'
+require 'json'
+
+module TestManagementService
+  class AccountServlet < WEBrick::HTTPServlet::AbstractServlet
+    def initialize(server, account_manager)
+      super server
+      @account_manager = account_manager
+    end
+
+    def do_GET (request, response)
+      case request.path_info
+      when "/request"
+        account = @account_manager.claim_account
+        if account.nil?
+          response.status = 409
+          response.body = 'No account currently available'
+        else
+          response.status = 200
+          response.header['content-type'] = 'application/json'
+          response.body = JSON.dump(account)
+        end
+      when "/release"
+        query_string = CGI.parse(request.query_string)
+        @account_manager.release_account(query_string['account_id'].first.to_i)
+        response.status = 202
+      else
+        response.status = 404
+        response.body = "Path #{request.path} not found"
+      end
+    end
+  end
+end

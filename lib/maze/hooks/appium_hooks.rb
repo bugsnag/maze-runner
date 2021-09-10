56ddcfc22435562e29765d6315ebf73e29d3e2ea
Alex Moinet <alex@bugsnag.com>
2021-09-10 10:22:31 +0100

    BitBar/TMS: Minor fixes to comments and format, minor optimisations

--- lib/maze/hooks/appium_hooks.rb
+++ lib/maze/hooks/appium_hooks.rb
@@ -24,11 +24,11 @@ module Maze
           config.app = Maze::BitBarUtils.upload_app config.access_key,
                                                     config.app
           Maze::BitBarUtils.start_local_tunnel config.bb_local,
-                                              config.username,
-                                              config.access_key
+                                               config.username,
+                                               config.access_key
           config.capabilities = Maze::Capabilities.for_bitbar_device config.access_key,
-                                                                    config.device,
-                                                                    config.capabilities_option
+                                                                     config.device,
+                                                                     config.capabilities_option
           config.capabilities['bitbar_app'] = config.app
           config.capabilities['bundleId'] = config.app_bundle_id
         elsif config.farm == :local

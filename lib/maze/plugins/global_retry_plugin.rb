08cd2ae6a9a7b9b15d3177acb96d5d04e6dbe2ba
Alex Moinet <alex@bugsnag.com>
2021-10-13 10:21:56 +0100

    Retry Plugin: Add some debugging for insight into tests

--- lib/maze/plugins/global_retry_plugin.rb
+++ lib/maze/plugins/global_retry_plugin.rb
@@ -7,11 +7,14 @@ module Maze
     class GlobalRetryPlugin < Cucumber::Core::Filter.new(:configuration)
 
       def test_case(test_case)
+        pp "Test_case starting for #{test_case}"
         configuration.on_event(:test_case_finished) do |event|
           # Guard for the test_case having failed
+          pp "Testing value of #{event.result.failed}"
           next unless event.test_case == test_case && event.result.failed?
 
           # Guard to check if the case should be retried
+          pp "Testing should_retry?"
           next unless Maze::RetryHandler.should_retry?(test_case, event)
 
           # Retry the test_case

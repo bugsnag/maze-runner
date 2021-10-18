c6700ad365513f5e65b31ea11ffcb7342ac1e273
Alex Moinet <alex@bugsnag.com>
2021-10-18 08:52:17 +0100

    Retry Plugin: Better method of accessing configuration options

--- lib/maze/plugins/global_retry_plugin.rb
+++ lib/maze/plugins/global_retry_plugin.rb
@@ -2,6 +2,13 @@
 
 require 'cucumber/core/filter'
 
+# Required to access the options
+module Cucumber
+  class Configuration
+    attr_accessor :options
+  end
+end
+
 module Maze
   module Plugins
     class GlobalRetryPlugin < Cucumber::Core::Filter.new(:configuration)
@@ -13,7 +20,7 @@ module Maze
           next unless event.test_case == test_case
 
           # Set retry to 0
-          set_retry_count(configuration, 0)
+          configuration.options[:retry] = 0
 
           # Guard to check if the case should be retried
           should_retry = event.result.failed? && Maze::RetryHandler.should_retry?(test_case, event)
@@ -21,17 +28,10 @@ module Maze
           next unless should_retry
 
           # Set retry to 1
-          set_retry_count(configuration, 1)
+          configuration.options[:retry] = 1
         end
 
         super
-      end
-
-      def set_retry_count(configuration, count)
-        pp "Retry to #{count}"
-        opts = configuration.instance_variable_get(:@options)
-        opts[:retry] = count
-        configuration.instance_variable_set(:@options, opts)
       end
     end
   end

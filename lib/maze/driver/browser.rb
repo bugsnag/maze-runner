9de86b42df2bf32dcf6b08f34817625ceea5652f
Alex Moinet <alex@bugsnag.com>
2022-06-23 16:54:41 +0100

    Browser driver update: Re-implement with improved error logging

--- lib/maze/driver/browser.rb
+++ lib/maze/driver/browser.rb
@@ -13,21 +13,10 @@ module Maze
       def initialize(driver_for, selenium_url=nil, capabilities=nil)
         capabilities.merge! project_name_capabilities
         @capabilities = capabilities
+        @driver_for = driver_for
+        @selenium_url = selenium_url
 
-        wait = Maze::Wait.new(interval: 10, timeout: 60)
-        success = wait.until do
-          begin
-            create_driver(driver_for, selenium_url)
-            true
-          rescue
-            false
-          end
-        end
-
-        unless success
-          $logger.error 'Selenium driver failed to start after 6 attempts in 60 seconds'
-          raise RuntimeError.new('Appium driver failed to start in 60 seconds')
-        end
+        start_driver
       end
 
       def find_element(*args)
@@ -91,7 +80,39 @@ module Maze
         }
       end
 
+      # Restarts the underlying-driver in the case an unrecoverable error occurs
+      #
+      # @param attempts [Integer] The number of times we should retry a failed attempt (defaults to 6)
+      def restart_driver(attempts=6)
+        # Remove the old driver
+        @driver.quit
+        @driver = nil
+
+        start_driver(attempts)
+      end
+
       private
+
+      # Attempts to create a new selenium driver a given number of times
+      #
+      # @param attempts [Integer] The number of times we should retry a failed attempt (defaults to 6)
+      def start_driver(attempts=6)
+        timeout = attempts * 10
+        wait = Maze::Wait.new(interval: 10, timeout: timeout)
+        success = wait.until do
+          begin
+            create_driver(@driver_for, @selenium_url)
+          rescue => error
+            $logger.warn "#{error.class} occurred with message: #{error.message}"
+          end
+          @driver
+        end
+
+        unless success
+          $logger.error "Selenium driver failed to start after #{attempts} attempts in #{timeout} seconds"
+          raise RuntimeError.new("Appium driver failed to start in #{timeout} seconds")
+        end
+      end
 
       # Creates and starts the selenium driver
       def create_driver(driver_for, selenium_url=nil)
@@ -109,7 +130,6 @@ module Maze
           @driver = driver
         rescue => error
           $logger.warn "Selenium driver failed to start in #{(Time.now - time).to_i}s"
-          $logger.warn "#{error.class} occurred with message: #{error.message}"
           raise error
         end
       end

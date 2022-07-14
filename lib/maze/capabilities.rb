f87394ff754671dc9b8a9e7cad7734f0bdc863f0
Alex Moinet <alex@bugsnag.com>
2022-07-14 21:14:54 +0100

    Add some debugging

--- lib/maze/capabilities.rb
+++ lib/maze/capabilities.rb
@@ -75,6 +75,8 @@ module Maze
         browsers = YAML.safe_load(File.read("#{__dir__}/browsers_bb.yml"))
         capabilities.merge! browsers[browser_type]
         capabilities.merge! JSON.parse(capabilities_option)
+        pp 'DEBUG: CAPS'
+        pp capabilities
         capabilities
       end
 

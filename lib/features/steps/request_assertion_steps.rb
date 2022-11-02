5dae3d8bc856e8966a79ca48ce9245b9e4ea68ca
Alex Moinet <alex@bugsnag.com>
2022-11-02 16:07:47 +0000

    Fix incorrect assertion logic on schema validation

--- lib/features/steps/request_assertion_steps.rb
+++ lib/features/steps/request_assertion_steps.rb
@@ -39,7 +39,9 @@ def verify_schema_matches(list, list_name)
   request_schema_errors = list.all.map { |request| request[:schema_errors] }
   passed = true
   request_schema_errors.each.with_index(1) do |schema_errors, index|
-    unless schema_errors.nil? || schema_errors.size == 0
+    next if schema_errors.nil?
+
+    if schema_errors.size > 0
       passed = false
       $stdout.puts "\n"
       $stdout.puts "\e[31m--- #{list_name} #{index} failed validation with the following errors:\e[0m"
@@ -48,7 +50,9 @@ def verify_schema_matches(list, list_name)
     end
   end
 
-  raise Test::Unit::AssertionFailedError.new 'The received payloads did not match the endpoint schema.  A full list of the errors can be found above'
+  unless passed
+    raise Test::Unit::AssertionFailedError.new 'The received payloads did not match the endpoint schema.  A full list of the errors can be found above'
+  end
 end
 
 #

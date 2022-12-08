b5eb9529f5daebec6c7892aa39a1ce5f982736c2
Alex Moinet <alex@bugsnag.com>
2022-12-08 13:44:57 +0000

    Correctly guard against no validation errors

--- lib/features/steps/request_assertion_steps.rb
+++ lib/features/steps/request_assertion_steps.rb
@@ -39,9 +39,8 @@ def verify_schema_matches(list, list_name)
   request_schema_results = list.all.map { |request| request[:schema_errors] }
   passed = true
   request_schema_results.each.with_index(1) do |schema_results, index|
+    next unless schema_results.validation_errors
     schema_errors = schema_results.validation_errors
-    next if schema_errors.nil?
-
     if schema_errors.size > 0
       passed = false
       $stdout.puts "\n"

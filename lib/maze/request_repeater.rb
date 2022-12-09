5cb263e5cfbca4e907e5f739079d53e2bf9df8aa
Steve Kirkland-Walton <steve@bugsnag.com>
2022-12-09 22:22:35 +0000

    Tidy-up

--- lib/maze/request_repeater.rb
+++ lib/maze/request_repeater.rb
@@ -10,24 +10,20 @@ module Maze
     # @param request [HTTPRequest] The request to be repeated
     def repeat(request)
 
-      # Do not forward internal errors
+      # TODO: Forwarding of internal errors to be considered later
       return if request.header.keys.any? { |key| key.downcase == 'bugsnag-internal-error' }
-
-      $logger.info "request.header.key?('Bugsnag-Api-Key'): #{request.header.key?('bugsnag-api-key')}"
 
       http = Net::HTTP.new(@url.host)
       bugsnag_request = Net::HTTP::Post.new(@url.path)
       bugsnag_request['Content-Type'] = 'application/json'
 
       # Set all Bugsnag headers that are present
-      request.header.each {|key,value|
-        $logger.info "#{key} = #{value}"
-        bugsnag_request[key] = value if key.downcase.start_with? 'bugsnag-'
-      }
+      bugsnag_request.body = request.body
+      request.header.each {|key,value| bugsnag_request[key] = value if key.downcase.start_with? 'bugsnag-' }
       bugsnag_request['bugsnag-api-key'] = Maze.config.repeater_api_key
+
       # TODO Also overwrite apiKey in the payload, if present, and recalculate the integrity header
 
-      bugsnag_request.body = request.body
       http.request(bugsnag_request)
     end
   end

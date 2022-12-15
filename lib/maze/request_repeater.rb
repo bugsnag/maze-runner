46e9577400ea1e38b7bcf6b6d662448ce2a4174f
Steve Kirkland-Walton <steve@bugsnag.com>
2022-12-15 10:22:56 +0000

    Push URI creation into method

--- lib/maze/request_repeater.rb
+++ lib/maze/request_repeater.rb
@@ -4,7 +4,7 @@ module Maze
 
     # @param request_type [String] The type of request being handled by this repeater
     def initialize(request_type)
-      @request_type = request_type
+      @url = url_for_request_type(request_type)
     end
 
     # @param request [HTTPRequest] The request to be repeated
@@ -13,9 +13,8 @@ module Maze
       # TODO: Forwarding of internal errors to be considered later
       return if request.header.keys.any? { |key| key.downcase == 'bugsnag-internal-error' }
 
-      url = URI.parse(url_for_request_type)
-      http = Net::HTTP.new(url.host)
-      bugsnag_request = Net::HTTP::Post.new(url.path)
+      http = Net::HTTP.new(@url.host)
+      bugsnag_request = Net::HTTP::Post.new(@url.path)
       bugsnag_request['Content-Type'] = 'application/json'
 
       # Set all Bugsnag headers that are present
@@ -30,12 +29,13 @@ module Maze
 
     private
 
-    def url_for_request_type
-      case @request_type
-      when :errors then 'https://notify.bugsnag.com/'
-      when :sessions then 'https://sessions.bugsnag.com/'
-      when :traces then 'https://otlp.bugsnag.com/v1/traces'
-      end
+    def url_for_request_type(request_type)
+      url = case request_type
+            when :errors then 'https://notify.bugsnag.com/'
+            when :sessions then 'https://sessions.bugsnag.com/'
+            when :traces then 'https://otlp.bugsnag.com/v1/traces'
+            end
+      URI.parse(url)
     end
   end
 end

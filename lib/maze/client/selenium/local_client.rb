36f70dcc79b993700bcc5809404d53129f5286f0
Steve Kirkland-Walton <steve@bugsnag.com>
2022-12-23 14:45:38 +0000

    Refactor starting of Appium client

--- lib/maze/client/selenium/local_client.rb
+++ lib/maze/client/selenium/local_client.rb
@@ -2,17 +2,9 @@ module Maze
   module Client
     module Selenium
       class LocalClient < BaseClient
-        def prepare_session
-          # Nothing to do
-        end
-
         def start_session
           Maze.driver = Maze::Driver::Browser.new Maze.config.browser.to_sym
           Maze.driver.start_driver
-        end
-
-        def log_session_info
-          # Nothing to do
         end
 
         def stop_session

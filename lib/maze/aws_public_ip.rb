e10e1efc66145d7ed2add975fca0ed5a88d4c11b
Steve Kirkland-Walton <steve@bugsnag.com>
2023-01-11 17:20:38 +0000

    Add class to determine public ip/port

---
+++ lib/maze/aws_public_ip.rb
@@ -0,0 +1,47 @@
+module Maze
+  # Determines the public IP address and port when running on Buildkite with the Elastic CI Stack for AWS
+  class AwsPublicIp
+    attr_reader :address
+
+    def initialize
+      # This class is only relevant on Buildkite
+      return unless ENV['BUILDKITE']
+
+      ip = determine_public_ip
+      port = determine_public_port
+
+      @address = "#{ip}:#{port}"
+    end
+
+    # Determines the public IP address of the running AWS instance
+    def determine_public_ip
+      `curl --silent -XGET http://169.254.169.254/latest/meta-data/public-ipv4`
+    end
+
+    # Determines the external port of the running Docker container that's associated with the port of the mock server
+    def determine_public_port
+      port = 0
+      count = 0
+
+      # Give up after 30 seconds
+      while port == 0 && count < 30 do
+        hostname = ENV['HOSTNAME']
+        command = "curl --silent -XGET --unix-socket /var/run/docker.sock http://localhost/containers/#{hostname}/json"
+        result = Maze::Runner.run_command(command)
+        if result[1] == 0
+          begin
+            json_string = result[0][0].strip
+            json_result = JSON.parse(json_string)
+            port = json_result['NetworkSettings']['Ports']["#{Maze.config.port}/tcp"][0]['HostPort']
+          rescue StandardError
+            $logger.error "Unable to parse public port from: #{json_string}"
+            return 0
+          end
+        end
+
+        count += 1
+        sleep 1 if port == 0 && count < 30
+      end
+    end
+  end
+end

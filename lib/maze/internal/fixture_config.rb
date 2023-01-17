f4b29a8329bdb7c4825866226947ff4f5fb4e5bd
Steve Kirkland-Walton <steve@bugsnag.com>
2023-01-17 14:23:49 +0000

    Ensure working folder exists

--- lib/maze/internal/fixture_config.rb
+++ lib/maze/internal/fixture_config.rb
@@ -5,7 +5,8 @@ module Maze
     # Handles the Maze Runner configuration file for test fixtures
     class FixtureConfig
       DEVICE_FILENAME = 'fixture_config.json'
-      WORKING_FILE = File.join('maze_working', DEVICE_FILENAME)
+      WORKING_DIRECTORY = 'maze_working'
+      WORKING_FILE = File.join(WORKING_DIRECTORY, DEVICE_FILENAME)
 
       def initialize
         @config = {}
@@ -16,6 +17,7 @@ module Maze
       end
 
       def write
+        FileUtils.makedirs(WORKING_DIRECTORY)
         File.open(WORKING_FILE, 'w+') do |file|
           file.write(JSON.pretty_generate(@config))
         end

8975f8421ce697f5acdbb7483cceb3c11f60b012
Steve Kirkland-Walton <steve@bugsnag.com>
2023-01-24 12:49:00 +0000

    Only write fixture config on modern mobiles

--- lib/maze/client/appium/bs_client.rb
+++ lib/maze/client/appium/bs_client.rb
@@ -15,8 +15,15 @@ module Maze
           end
         end
 
-        def maze_address
-          "bs-local.com:#{Maze.config.port}"
+        def start_scenario
+          unless Maze.config.legacy_driver?
+            # Write Maze's address to file and push to the device
+            maze_address = "bs-local.com:#{Maze.config.port}"
+            Maze::Api::Appium::FileManager.new.write_app_file(JSON.generate({ maze_address: maze_address }),
+                                                              FIXTURE_CONFIG)
+          end
+
+          super
         end
 
         def device_capabilities

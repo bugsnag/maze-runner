1f8bfa48f9080067da26f8ce4398b5bd61c9ff11
Steve Kirkland-Walton <steve@bugsnag.com>
2023-02-16 17:56:16 +0000

    Read test config

--- test/fixtures/android-appium-test/app/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
+++ test/fixtures/android-appium-test/app/src/main/java/com/bugsnag/android/mazerunner/MainActivity.kt
@@ -7,13 +7,19 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import com.bugsnag.android.*
+import java.io.File
 import java.lang.Exception
+import java.lang.Thread
 import java.net.URL
 import kotlin.concurrent.thread
 import org.json.JSONObject
 import org.json.JSONTokener
 
+const val CONFIG_FILE_TIMEOUT = 5000
+
 class MainActivity : AppCompatActivity() {
+
+    var mazeAddress: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +49,49 @@ class MainActivity : AppCompatActivity() {
         startBugsnag()
     }
 
+    private fun setMazeRunnerAddress() {
+        val context = getApplicationContext()
+        val externalFilesDir = context.getExternalFilesDir(null)
+        val configFile = File(externalFilesDir, "fixture_config.json")
+        log("Attempting to read Maze Runner address from config file ${configFile.path}")
+
+        // Poll for the fixture config file
+        val pollEnd = System.currentTimeMillis() + CONFIG_FILE_TIMEOUT
+        while (System.currentTimeMillis() < pollEnd) {
+            if (configFile.exists()) {
+                val fileContents = configFile.readText()
+                val fixtureConfig = runCatching { JSONObject(fileContents) }.getOrNull()
+                mazeAddress = getStringSafely(fixtureConfig, "maze_address")
+                if (!mazeAddress.isNullOrBlank()) {
+                    log("Maze Runner address set from config file: $mazeAddress")
+                    break
+                }
+            }
+
+            Thread.sleep(250)
+        }
+
+        // Assume we are running in legacy mode on BrowserStack
+        if (mazeAddress.isNullOrBlank()) {
+            log("Failed to read Maze Runner address from config file, reverting to legacy BrowserStack address")
+            mazeAddress = "bs-local.com:9339"
+        }
+    }
+
+    // As per JSONObject.getString but returns and empty string rather than throwing if not present
+    private fun getStringSafely(jsonObject: JSONObject?, key: String): String {
+        return jsonObject?.optString(key) ?: ""
+    }
+
+    private fun log(msg: String) {
+        Log.d("BugsnagMazeRunner", msg)
+    }
+
     private fun startBugsnag() {
+        if (mazeAddress == null) setMazeRunnerAddress()
         val config = Configuration("12312312312312312312312312312312")
         config.autoTrackSessions = false
-        config.setEndpoints(EndpointConfiguration("http://maze-local:9339/notify", "http://maze-local:9339/sessions"))
+        config.setEndpoints(EndpointConfiguration("http://$mazeAddress/notify", "http://$mazeAddress/sessions"))
         config.addOnError(OnErrorCallback { event ->
             event.addMetadata("test", "boolean_false", false)
             event.addMetadata("test", "boolean_true", true)
@@ -59,5 +104,4 @@ class MainActivity : AppCompatActivity() {
 
         Bugsnag.start(this, config)
     }
-
 }

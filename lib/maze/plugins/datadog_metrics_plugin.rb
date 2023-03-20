e5765c4ded914c00a2dc1ab62ff748cd10957475
Alex Moinet <alex@bugsnag.com>
2023-03-20 15:53:25 +0000

    Add some debugging

--- lib/maze/plugins/datadog_metrics_plugin.rb
+++ lib/maze/plugins/datadog_metrics_plugin.rb
@@ -23,14 +23,16 @@ module Maze
         end
 
         def initialize_stats_dog
+          pp 'Starting stats dog'
           tags = []
           tags << Maze.config.device.to_s if Maze.config.device
           tags << Maze.config.farm.to_s if Maze.config.farm
-          @stats_dog = Datadog::Statsd.new(aws_instance_ip, 8125, tags: tags, namespace: 'maze-runner')
+          @stats_dog = Datadog::Statsd.new(aws_instance_ip, 8125, tags: tags, namespace: 'maze-runner', single_thread: true)
+          pp @stats_dog
         end
 
         def aws_instance_ip
-          `curl --silent -XGET http://169.254.169.254/latest/meta-data/local-ipv4`
+          `curl --silent -XGET http://169.254.169.254/latest/meta-data/public-ipv4`
         end
       end
     end

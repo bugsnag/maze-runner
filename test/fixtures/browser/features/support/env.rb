915008577e6b88116b8bec002581488d0bf41bd9
Steve Kirkland-Walton <steve@bugsnag.com>
2023-04-28 17:58:03 +0100

    Use the built-in document server

--- test/fixtures/browser/features/support/env.rb
+++ test/fixtures/browser/features/support/env.rb
@@ -1,25 +1,9 @@
 require 'yaml'
 
-def get_test_url(path)
-  if Maze.config.aws_public_ip
-    "http://#{Maze.public_address}#{path}"
-  else
-    "http://bs-local.com:#{FIXTURES_SERVER_PORT}#{path}"
-  end
-
-end
-
-BeforeAll do
+Maze.hooks.before_all do
   Maze.config.receive_no_requests_wait = 15
   Maze.config.enforce_bugsnag_integrity = false
-
-  FIXTURES_SERVER_PORT = '9020'
-  DEV_NULL = Gem.win_platform? ? 'NUL' : '/dev/null'
-    pid = Process.spawn({"PORT"=>FIXTURES_SERVER_PORT},
-                        'ruby features/lib/server.rb',
-                        :out => DEV_NULL,
-                        :err => DEV_NULL)
-  Process.detach(pid)
+  Maze.config.document_server_root = 'features/fixtures'
 end
 
 at_exit do

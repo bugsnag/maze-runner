3aa8bf6f75ee1f41b6e9eb7c625e6e85a5a8692a
Steve Kirkland-Walton <steve@bugsnag.com>
2023-09-27 16:59:34 +0100

    Notify consistently in each error case

--- lib/maze/servlets/servlet.rb
+++ lib/maze/servlets/servlet.rb
@@ -102,9 +102,9 @@ module Maze
         set_response_header response.header
         response.status = post_status_code
       rescue JSON::ParserError => e
-        Bugsnag.notify e
         msg = "Unable to parse request as JSON: #{e.message}"
         if Maze.config.captured_invalid_requests.include? @request_type
+          Bugsnag.notify e
           $logger.error msg
           Server.invalid_requests.add({
             reason: msg,
@@ -115,9 +115,10 @@ module Maze
           $logger.warn msg
         end
       rescue StandardError => e
+        msg = "Invalid #{@request_type} request: #{e.message}"
         if Maze.config.captured_invalid_requests.include? @request_type
           Bugsnag.notify e
-          $logger.error "Invalid #{@request_type} request: #{e.message}"
+          $logger.msg
           Server.invalid_requests.add({
             invalid: true,
             reason: e.message,
@@ -128,7 +129,7 @@ module Maze
             }
           })
         else
-          $logger.warn "Invalid #{@request_type} request: #{e.message}"
+          $logger.warn msg
         end
       end
 

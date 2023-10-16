98a2b11a6783a68f966ef439746757998942deff
Steve Kirkland-Walton <steve@bugsnag.com>
2023-10-16 11:14:21 +0100

    Avoid shell interpretation of characters in file names in Maze::MacosUtils.capture_screen

--- test/fixtures/framework/features/steps/steps.rb
+++ test/fixtures/framework/features/steps/steps.rb
@@ -14,13 +14,13 @@ end
 # @step_input key [String] The environment variable
 # @step_input value [String] The intended value of the environment variable
 Then('the Runner.environment entry for {string} equals {string}') do |key, expected_value|
-  assert_equal(expected_value, Maze::Runner.environment[key])
+  Maze.check.equal(expected_value, Maze::Runner.environment[key])
 end
 
 # Verifies that an environment variable is not set.
 #
 # @step_input key [String] The environment variable
 Then('the Runner.environment entry for {string} is null') do |key|
-  assert_nil(Maze::Runner.environment[key])
+  Maze.check.nil(Maze::Runner.environment[key])
 end
 

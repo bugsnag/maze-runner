d03de6a0b4680b0f7c3d6897525f9c809305b282
Alex Moinet <alex@bugsnag.com>
2024-01-05 11:17:09 +0000

    Add method to allow pulling of files via appium client

--- lib/maze/api/appium/file_manager.rb
+++ lib/maze/api/appium/file_manager.rb
@@ -25,6 +25,28 @@ module Maze
           $logger.trace "Pushing file to '#{path}' with contents: #{contents}"
           @driver.push_file(path, contents)
         end
+
+        # Attempts to retrieve a given file from the device (using Appium).  The default location for the file will be
+        # the app's documents directory for iOS. On Android, it will be /sdcard/Android/data/<app-id>/files unless
+        # Maze.config.android_app_files_directory has been set.
+        # @param filename [String] Name (with no path) of the file to be retrieved from the device
+        # @param directory [String] Directory on the device where the file is located (optional)
+        def read_app_file(filename, directory = nil)
+          if directory
+            path = directory
+          else
+            path = case Maze::Helper.get_current_platform
+                  when 'ios'
+                    "@#{@driver_app_id}/Documents/#{filename}"
+                  when 'android'
+                    dir = Maze.config.android_app_files_directory || "/sdcard/Android/data/#{@driver.app_id}/files"
+                    "#{dir}/#{filename}"
+                  end
+          end
+
+          $logger.trace "Attempting to read file from '#{path}'"
+          file = @driver.pull_file(path)
+        end
       end
     end
   end

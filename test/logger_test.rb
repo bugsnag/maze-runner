24bc8e994d824fb8553115c00eb8142fe1208d1a
Alex Moinet <alex@bugsnag.com>
2024-01-10 16:01:34 +0000

    Update unit tests to allow for dual logging

--- test/logger_test.rb
+++ test/logger_test.rb
@@ -12,23 +12,25 @@ class LoggerTest < Test::Unit::TestCase
     reset_logger!
   end
 
+  
   def test_it_logs_messages
     io = StringIO.new
-
+    
     logger = Maze::Logger.instance
     logger.level = Logger::DEBUG
     logger.reopen(io)
-
+    
     io.rewind
     assert_empty(io.read)
-
+    
     Timecop.freeze(Time.utc(2023, 1, 2, 3, 4, 5)) { logger.debug('hello') }
     Timecop.freeze(Time.utc(2023, 1, 2, 6, 7, 8)) { logger.info('hello') }
     Timecop.freeze(Time.utc(2023, 1, 2, 9, 10, 11)) { logger.warn('hello') }
     Timecop.freeze(Time.utc(2023, 1, 2, 12, 13, 14)) { logger.error('hello') }
     Timecop.freeze(Time.utc(2023, 1, 2, 15, 16, 17)) { logger.fatal('hello') }
     Timecop.freeze(Time.utc(2023, 1, 2, 18, 19, 20)) { logger.unknown('hello') }
-
+    Timecop.freeze(Time.utc(2023, 1, 2, 21, 22, 23)) { logger.trace('hello') }
+    
     # Expect duplicates due to two loggers being active
     expected = [
       "\e[2m[03:04:05]\e[0m DEBUG: hello\n",
@@ -44,14 +46,52 @@ class LoggerTest < Test::Unit::TestCase
       "\e[2m[18:19:20]\e[0m   ANY: hello\n",
       "\e[2m[18:19:20]\e[0m   ANY: hello\n"
     ]
-
+    
     io.rewind
     assert_equal(expected, io.readlines)
   end
 
+  def test_dual_logger_defaults
+    file_io = StringIO.new
+    stdout_io = StringIO.new
+
+    logger = Maze::Logger.instance
+    logger.stdout_logger.reopen(stdout_io)
+    logger.file_logger.reopen(file_io)
+
+    stdout_io.rewind
+    assert_empty(stdout_io.read)
+
+    file_io.rewind
+    assert_empty(file_io.read)
+
+    Timecop.freeze(Time.utc(2023, 1, 2, 3, 4, 5)) { logger.trace('hello') }
+    Timecop.freeze(Time.utc(2023, 1, 2, 6, 7, 8)) { logger.debug('hello') }
+    Timecop.freeze(Time.utc(2023, 1, 2, 9, 10, 11)) { logger.info('hello') }
+    Timecop.freeze(Time.utc(2023, 1, 2, 12, 13, 14)) { logger.warn('hello') }
+
+    file_expected = [
+      "\e[2m[03:04:05]\e[0m TRACE: hello\n",
+      "\e[2m[06:07:08]\e[0m DEBUG: hello\n",
+      "\e[2m[09:10:11]\e[0m  INFO: hello\n",
+      "\e[2m[12:13:14]\e[0m  WARN: hello\n"
+    ]
+
+    file_io.rewind
+    assert_equal(file_expected, file_io.readlines)
+
+    stdout_expected = [
+      "\e[2m[09:10:11]\e[0m  INFO: hello\n",
+      "\e[2m[12:13:14]\e[0m  WARN: hello\n"
+    ]
+
+    stdout_io.rewind
+    assert_equal(stdout_expected, stdout_io.readlines)
+  end
+  
   def test_the_datetime_format_can_be_changed
     io = StringIO.new
-
+    
     logger = Maze::Logger.instance
     logger.level = Logger::DEBUG
     logger.reopen(io)

7a97be68be1b23821bf47461caf9b526d64bc0f3
Steve Kirkland-Walton <steve@bugsnag.com>
2024-06-21 17:24:00 +0100

    Update changelog

--- lib/maze.rb
+++ lib/maze.rb
@@ -7,7 +7,7 @@ require_relative 'maze/timers'
 # Glues the various parts of MazeRunner together that need to be accessed globally,
 # providing an alternative to the proliferation of global variables or singletons.
 module Maze
-  VERSION = '9.9.0'
+  VERSION = '9.9.1'
 
   class << self
     attr_accessor :check, :driver, :internal_hooks, :mode, :start_time, :dynamic_retry, :public_address,

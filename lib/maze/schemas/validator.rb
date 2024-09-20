56b26b53eb940a5dd1305ae47eacc1f0e360aaa7
Alex Moinet <alex@bugsnag.com>
2024-09-20 09:34:44 +0100

    Allow validators to run together

--- lib/maze/schemas/validator.rb
+++ lib/maze/schemas/validator.rb
@@ -18,36 +18,30 @@ module Maze
         # @param list_name [String] The name of the payload list for received requests
         def validate_payload_elements(list, list_name)
           # Test to see if a custom validator exists for the list
-          if Maze.config.custom_validators&.key?(list_name)
-            custom_validator = true
-            validator_class = Maze::Schemas::ConfigValidator
+          custom_validator = Maze.config.custom_validators&.key?(list_name)
+          validator_class = case list_name
+          when 'trace', 'traces'
+            Maze::Schemas::TraceValidator
+          when 'error', 'errors'
+            Maze::Schemas::ErrorValidator
           else
-            custom_validator = false
-            validator_class = case list_name
-            when 'trace', 'traces'
-              Maze::Schemas::TraceValidator
-            when 'error', 'errors'
-              Maze::Schemas::ErrorValidator
-            else
-              nil
-            end
+            nil
           end
 
-          if validator_class
-            validators = list.all.map do |request|
-              # TODO: Implement generically in class and handle there?
-              if custom_validator
-                validator = validator_class.new(request, Maze.config.custom_validators[list_name])
-              else
-                validator = validator_class.new(request)
-              end
-              validator.validate
-              validator
-            end
+          list_validators = list.all.map do |request|
+            payload_validators = []
+            payload_validators << Maze::Schemas::ConfigValidator.new(request, Maze.config.custom_validators[list_name]) if custom_validator
+            payload_validators << validator_class.new(request) if validator_class
 
-            return if validators.all? { |validator| validator.success }
-            validators.each.with_index(1) do |validator, index|
+            payload_validators.each { |validator| validator.validate }
+            payload_validators
+          end
+
+          failing = false
+          list_validators.each.with_index(1) do |validators, index|
+            validators.each do |validator|
               unless validator.success
+                failing = true
                 $stdout.puts "\n"
                 $stdout.puts "\e[31m--- #{list_name} #{index} failed validation with the following errors:\e[0m"
                 validator.errors.each do |error|
@@ -56,8 +50,8 @@ module Maze
                 $stdout.puts "\n"
               end
             end
-            raise Test::Unit::AssertionFailedError.new("One or more #{list_name} payloads failed validation.  A full list of the errors can be found above")
           end
+          raise Test::Unit::AssertionFailedError.new("One or more #{list_name} payloads failed validation.  A full list of the errors can be found above") if failing
         end
       end
     end

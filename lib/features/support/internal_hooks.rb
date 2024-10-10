d02ca619e246e311f1201f9db7d36f9cbed3f505
Alex Moinet <alex@bugsnag.com>
2024-10-10 13:40:40 +0100

    Quick test of hook timing

--- lib/features/support/internal_hooks.rb
+++ lib/features/support/internal_hooks.rb
@@ -243,6 +243,10 @@ After do |_scenario|
   Maze::ErrorCaptor.instance.reset_captured_errors
 end
 
+After do |_scenario|
+  pp "internal_hooks AFTER"
+end
+
 # After all tests
 AfterAll do
   # Ensure the logger output is in the correct location

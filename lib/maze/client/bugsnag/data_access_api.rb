6a54336c125a43faec2b7af5eda35d1519782d2c
Sachin Pande <sazap10@gmail.com>
2024-10-17 11:15:22 +0100

    fix project id in  get_event

--- lib/maze/client/bugsnag/data_access_api.rb
+++ lib/maze/client/bugsnag/data_access_api.rb
@@ -6,7 +6,8 @@ module Maze
       # An abstraction for the underlying Bugsnag data access
       class DataAccessApi
         # @param api_key The Bugsnag API key
-        # @param endpoint The endpoint to use for the Bugsnag Data Access API
+        # @param endpoint The endpoint to use for the Bugsnag Data Access API#
+        # @param project_id The project ID to use for the Bugsnag Data Access API
         def initialize
           @api_key = Maze.config.bugsnag_data_access_api_key
           @endpoint = Maze.config.bugsnag_data_access_api_endpoint
@@ -23,7 +24,7 @@ module Maze
         end
 
         def get_event(event_id)
-          @client.event(project_id, event_id)
+          @client.event(@project_id, event_id)
         end
       end
     end

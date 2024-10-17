8845acace0b556191358ec23efc6a5bd85e42c00
Sachin Pande <sazap10@gmail.com>
2024-10-17 11:22:00 +0100

    provide client config via hash

--- lib/maze/client/bugsnag/data_access_api.rb
+++ lib/maze/client/bugsnag/data_access_api.rb
@@ -12,15 +12,15 @@ module Maze
           @api_key = Maze.config.bugsnag_data_access_api_key
           @endpoint = Maze.config.bugsnag_data_access_api_endpoint
           @project_id = Maze.config.bugsnag_data_access_project_id
+          opts = {
+            api_key: @api_key,
+            endpoint: @endpoint,
+            project_id: @project_id
+          }
           if @endpoint.start_with?('http')
-            connection_options = { ssl: { verify: false } }
+            opts.connection_options = { ssl: { verify: false } }
           end
-          @client = Bugsnag::Api::Client.new(
-              api_key: @api_key,
-              endpoint: @endpoint,
-              auto_paginate: true,
-              connection_options: connection_options if connection_options
-            )
+          @client = Bugsnag::Api::Client.new(opts)
         end
 
         def get_event(event_id)

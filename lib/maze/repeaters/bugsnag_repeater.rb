55fd3c4c0fc2f40e2283206305c2d54a003d4a1c
Steve Kirkland <steve.kirkland@smartbear.com>
2024-10-17 12:13:34 +0100

    Hackathon - allow repeating of errors to Bugsnag development

--- lib/maze/repeaters/bugsnag_repeater.rb
+++ lib/maze/repeaters/bugsnag_repeater.rb
@@ -17,10 +17,11 @@ module Maze
       end
 
       def url_for_request_type
+        traces_endpoint = ENV['MAZE_REPEATER_TRACES_ENDPOINT'] || 'otlp.bugsnag.com/v1/traces'
         url = case @request_type
-              when :errors then 'https://notify.bugsnag.com/'
-              when :sessions then 'https://sessions.bugsnag.com/'
-              when :traces then "https://#{Maze.config.bugsnag_repeater_api_key}.otlp.bugsnag.com/v1/traces"
+              when :errors then ENV['MAZE_REPEATER_NOTIFY_ENDPOINT'] || 'https://notify.bugsnag.com/'
+              when :sessions then ENV['MAZE_REPEATER_SESSIONS_ENDPOINT'] || 'https://sessions.bugsnag.com/'
+              when :traces then "https://#{Maze.config.bugsnag_repeater_api_key}.#{traces_endpoint}"
               else return nil
               end
         URI.parse(url)

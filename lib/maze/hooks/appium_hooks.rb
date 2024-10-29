ee3c762e9b336e372cf169bf2a9e5b226eee018d
Alex Moinet <alex@bugsnag.com>
2024-10-29 15:13:56 +0000

    Add extra guard when terminating appium session app

--- lib/maze/hooks/appium_hooks.rb
+++ lib/maze/hooks/appium_hooks.rb
@@ -34,7 +34,7 @@ module Maze
           # commands from the previous scenario (in idempotent mode).
           begin
             Maze.driver.terminate_app Maze.driver.app_id
-          rescue Selenium::WebDriver::Error::UnknownError
+          rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::InvalidSessionIdError
             if Maze.config.appium_version && Maze.config.appium_version.to_f < 2.0
               $logger.warn 'terminate_app failed, using the slower but more forceful close_app instead'
               Maze.driver.close_app

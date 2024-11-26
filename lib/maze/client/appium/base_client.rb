607c3aef3fc061383d3badbdfaf7c1f43e983c3d
Josh Edney <j.edney17@gmail.com>
2024-11-26 15:55:13 +0000

    log app id

--- lib/maze/client/appium/base_client.rb
+++ lib/maze/client/appium/base_client.rb
@@ -25,6 +25,9 @@ module Maze
                                  unless app_id = Maze.driver.session_capabilities['CFBundleIdentifier']
                                     app_id = Maze.driver.session_capabilities['bundleID']
                                  end
+                                 $logger.Info "App ID: #{app_id}"
+                                 $logger.Info "App ID: #{Maze.driver.session_capabilities['bundleID']}"
+                                 $logger.Info "App ID: #{Maze.driver.session_capabilities['CFBundleIdentifier']}"
                                  app_id
                                end
 

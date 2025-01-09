54450d636877d1a225a22923a1b5cd16fa0e7956
Steve Kirkland <steve.kirkland@smartbear.com>
2025-01-09 14:56:43 +0000

    Return the file read rather than a boolean

--- lib/maze/api/appium/file_manager.rb
+++ lib/maze/api/appium/file_manager.rb
@@ -11,10 +11,11 @@ module Maze
         # Maze.config.android_app_files_directory has been set.
         # @param contents [String] Content of the file to be written
         # @param filename [String] Name (with no path) of the file to be written on the device
+        # @return [Boolean] Whether the file was successfully written to the device
         def write_app_file(contents, filename)
           if failed_driver?
             $logger.error 'Cannot write file to device - Appium driver failed.'
-            return
+            return false
           end
 
           path = case Maze::Helper.get_current_platform
@@ -23,6 +24,8 @@ module Maze
                  when 'android'
                    directory = Maze.config.android_app_files_directory || "/sdcard/Android/data/#{@driver.app_id}/files"
                    "#{directory}/#{filename}"
+                 else
+                   raise 'write_app_file is not supported on this platform'
                  end
 
           $logger.trace "Pushing file to '#{path}' with contents: #{contents}"
@@ -42,10 +45,11 @@ module Maze
         # Maze.config.android_app_files_directory has been set.
         # @param filename [String] Name (with no path) of the file to be retrieved from the device
         # @param directory [String] Directory on the device where the file is located (optional)
+        # @return [String, nil] The content of the file read, or nil
         def read_app_file(filename, directory = nil)
           if failed_driver?
             $logger.error 'Cannot read file from device - Appium driver failed.'
-            return
+            return nil
           end
 
           if directory
@@ -57,15 +61,16 @@ module Maze
                   when 'android'
                     dir = Maze.config.android_app_files_directory || "/sdcard/Android/data/#{@driver.app_id}/files"
                     "#{dir}/#{filename}"
+                  else
+                    raise 'read_app_file is not supported on this platform'
                   end
           end
 
           $logger.trace "Attempting to read file from '#{path}'"
           @driver.pull_file(path)
-          true
         rescue Selenium::WebDriver::Error::UnknownError => e
           $logger.error "Error reading file from device: #{e.message}"
-          false
+          nil
         rescue Selenium::WebDriver::Error::ServerError => e
           # Assume the remote appium session has stopped, so crash out of the session
           fail_driver

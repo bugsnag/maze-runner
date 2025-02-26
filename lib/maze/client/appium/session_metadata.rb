6ef80de13f7add90dfd11740e13352cd93af141e
Steve Kirkland <steve.kirkland@smartbear.com>
2025-02-26 23:10:49 +0000

    Report Appium session prototype

--- lib/maze/client/appium/session_metadata.rb
+++ lib/maze/client/appium/session_metadata.rb
@@ -2,6 +2,11 @@ module Maze
   module Client
     module Appium
       class SessionMetadata
+        def initialize
+          @success = false
+          @failure_message = 'Default failure message'
+        end
+
         attr_accessor :farm
         attr_accessor :device
         attr_accessor :success

15691bb91ace6e188c2e384797cb57a273f9c50f
Steve Kirkland-Walton <steve.kirkland-walton@SB-UK-GM2F9HN.fritz.box>
2025-04-10 12:14:27 +0100

    Fail scenario when Appium driver fails

--- lib/maze/hooks/appium_hooks.rb
+++ lib/maze/hooks/appium_hooks.rb
@@ -26,21 +26,23 @@ module Maze
       def after(scenario)
         manager = Maze::Api::Appium::AppManager.new
         if Maze.config.os == 'macos'
-          # Close the app - without the sleep, launching the app for the next scenario intermittently fails
+          # Close the app - without the sleep launching the app for the next scenario intermittently fails
           system("killall -KILL #{Maze.config.app} && sleep 1")
         elsif [:bb, :bs, :local].include? Maze.config.farm
-          # Reset the server to ensure that test fixtures cannot fetch
-          # commands from the previous scenario (in idempotent mode).
+          close_fallback = Maze.config.appium_version && Maze.config.appium_version.to_f < 2.0
           begin
-            manager.terminate
+            manager.terminate(!close_fallback)
           rescue Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::InvalidSessionIdError
-            if Maze.config.appium_version && Maze.config.appium_version.to_f < 2.0
+            if close_fallback
               $logger.warn 'terminate_app failed, using the slower but more forceful close_app instead'
               manager.close
             else
               $logger.warn 'terminate_app failed, future errors may occur if the application did not close remotely'
             end
           end
+
+          # Reset the server before relaunching the app to ensure that test fixtures cannot fetch
+          # commands from the previous scenario (in idempotent mode).
           Maze::Server.reset!
           manager.activate
         end

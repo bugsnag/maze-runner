cc0b5ca4310960724f3df71ede7d309b268f9ade
Steve Kirkland-Walton <steve.kirkland-walton@SB-UK-GM2F9HN.fritz.box>
2025-05-11 08:15:33 +0100

    Debug/force new image build

--- lib/utils/selenium_monkey_patch.rb
+++ lib/utils/selenium_monkey_patch.rb
@@ -4,10 +4,12 @@ module Selenium
     module Error
       class ServerError < StandardError
         def initialize(response)
+          $logger.info "In the monkey patch for ServerError"
           if response.is_a? String
             super(response)
           elsif response.is_a? Selenium::WebDriver::Remote::Response &&
                 response.payload.respond_to?("message")
+            $logger.info "It is a Selenium::WebDriver::Remote::Response: #{response.inspect}"
             super("Status code #{response.code}: #{response.payload.message}")
           else
             super(response.inspect)

b2d0d9c3709416630e1d298ad34461f32c2e41e7
Steve Kirkland <steve.kirkland@smartbear.com>
2025-12-10 19:49:09 +0000

    Corrections

--- lib/maze/client/selenium/base_client.rb
+++ lib/maze/client/selenium/base_client.rb
@@ -16,7 +16,7 @@ module Maze
             $logger.trace "Attempting to start Selenium driver with capabilities: #{config.capabilities.to_json}"
             $logger.trace "Attempt #{attempts}"
             begin
-              sanitized_url = Maze::Helper.sanitize_url(config.selenium_url)
+              sanitized_url = Maze::Helper.sanitize_url(selenium_url)
               $logger.info "Creating Selenium session with #{sanitized_url}..."
               Maze.driver = Maze::Driver::Browser.new(:remote, selenium_url, config.capabilities)
               Maze.driver.start_driver
